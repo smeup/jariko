fa91b17beb2d3021b30a40d49d55460b04c593f4
Federico Tomassetti <federico@tomassetti.me>
2019-03-12 13:49:59 +0100

    refine buildAst smoke tests

--- src/test/kotlin/com/smeup/rpgparser/parsing/RpgParserSmokeTest.kt
+++ src/test/kotlin/com/smeup/rpgparser/parsing/RpgParserSmokeTest.kt
@@ -3,6 +3,7 @@ package com.smeup.rpgparser.parsing
 import com.smeup.rpgparser.assertASTCanBeProduced
 import com.smeup.rpgparser.assertCanBeParsed
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class RpgParserSmokeTest {
 
@@ -43,16 +44,25 @@ class RpgParserSmokeTest {
 
     @Test
     fun buildAstForJD_001() {
-        assertASTCanBeProduced("JD_001")
+        val cu = assertASTCanBeProduced("JD_001")
+        assertEquals(10, cu.dataDefinitons.size)
+        assertEquals(4, cu.main.stmts.size)
+        assertEquals(7, cu.subroutines.size)
     }
 
     @Test
     fun buildAstForJD_002() {
-        assertASTCanBeProduced("JD_002")
+        val cu = assertASTCanBeProduced("JD_002")
+        assertEquals(18, cu.dataDefinitons.size)
+        assertEquals(4, cu.main.stmts.size)
+        assertEquals(10, cu.subroutines.size)
     }
 
     @Test
     fun buildAstForJD_003() {
-        assertASTCanBeProduced("JD_003")
+        val cu = assertASTCanBeProduced("JD_003")
+        assertEquals(17, cu.dataDefinitons.size)
+        assertEquals(4, cu.main.stmts.size)
+        assertEquals(6, cu.subroutines.size)
     }
 }
