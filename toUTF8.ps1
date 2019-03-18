7b34e676d2768fcce79d13b2560520c6a3f2b5f7
Franco Lombardo <f dot lombardo 69 at gmail dot com>
2019-03-18 18:19:35 +0100

    Added Power Shell Script for converting RPG sources to UTF-8

--- toUTF8.ps1
+++ toUTF8.ps1
@@ -1,4 +1,4 @@
-# Windows Power Shell script to convet RPG sources downloaded from AS400 to UTF8
+# Windows Power Shell script to convert RPG sources downloaded from AS400 to UTF8
 # The first parameter is the name of the file to convert
 param( [string] $infile = $(throw "Please specify a filename.") )
 
