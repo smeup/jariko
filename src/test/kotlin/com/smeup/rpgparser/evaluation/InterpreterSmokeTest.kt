0adc2970b21c9978b3144faf19f68bfc8c7fe7fd
Federico Tomassetti <federico@tomassetti.me>
2019-03-27 09:32:07 +0100

    create InterpreterSmokeTest

---
+++ src/test/kotlin/com/smeup/rpgparser/evaluation/InterpreterSmokeTest.kt
@@ -0,0 +1,34 @@
+package com.smeup.rpgparser.evaluation
+
+import com.smeup.rpgparser.*
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class InterpreterSmokeTest {
+
+    @Test
+    fun executeJD_001() {
+        val cu = assertASTCanBeProduced("JD_001")
+        execute(cu)
+    }
+
+    @Test
+    fun executeJD_002() {
+        val cu = assertASTCanBeProduced("JD_002")
+        execute(cu)
+    }
+
+    @Test
+    fun executeJD_003() {
+        val cu = assertASTCanBeProduced("JD_003")
+        execute(cu)
+    }
+
+    private fun execute(cu: CompilationUnit) {
+        val systemInterface = object : SystemInterface {
+
+        }
+        return Interpreter(systemInterface).execute(cu)
+    }
+
+}
