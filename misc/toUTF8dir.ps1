19dd82867d12a397ecf199d3f9376c466b1a705b
Franco Lombardo <f dot lombardo 69 at gmail dot com>
2019-04-01 17:38:13 +0200

    Added Powershell utility to convert to UTF8 all files in a directory

---
+++ misc/toUTF8dir.ps1
@@ -0,0 +1,9 @@
+# Windows Power Shell script to convert RPG sources downloaded from AS400 to UTF8
+# The first parameter is the name of the file to convert
+param( [string] $dirName = $(throw "Please specify a directory name.") )
+
+Get-ChildItem $dirName |
+        Foreach-Object {
+            $outfile = "$_.txt"
+            get-content -Path $_ | out-file $outfile -encoding utf8
+        }
