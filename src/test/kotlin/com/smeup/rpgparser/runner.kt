d3db15524a0d40e04cb8aa9da98b509ac57e72b5
Franco Lombardo <f dot lombardo 69 at gmail dot com>
2019-04-29 16:42:20 +0200

    Added runner class and gradle task

---
+++ src/test/kotlin/com/smeup/rpgparser/runner.kt
@@ -0,0 +1,38 @@
+package com.smeup.rpgparser
+
+import com.smeup.rpgparser.interpreter.*
+
+class ConsoleSystemInterface() : SystemInterface {
+    override fun findProgram(name: String): Program? {
+        return null
+    }
+
+    override fun display(value: String) {
+        println(value)
+    }
+}
+
+fun runPgm(exampleName: String, parameters: List<String>) {
+    val parseTreeRoot = RpgParserFacade().parse(inputStreamFor(exampleName)).root!!
+    val cu = parseTreeRoot.toAst(ToAstConfiguration(true))
+    cu.resolve()
+    val si = ConsoleSystemInterface()
+    execute(cu, toNameValueMap(cu.dataDefinitions, parameters), si)
+}
+
+private fun toNameValueMap(dataDefinitions: List<DataDefinition>, parameters: List<String>) : Map<String, Value> {
+    val values = parameters.map { parameter -> StringValue(parameter) }
+    return dataDefinitions
+                .map {dataDefinition -> dataDefinition.name }
+                .zip(values)
+                .toMap()
+}
+
+fun main(args : Array<String>) {
+    if (args.size == 0) {
+        println("Please provide the name of a .rpgle file to interpret")
+        return
+    }
+
+    runPgm(args[0], args.asList().subList(1, args.size))
+}
