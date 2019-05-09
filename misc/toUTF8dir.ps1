391abed7703a5a79b252a550f2c595d853cb9f3c
Franco Lombardo <f dot lombardo 69 at gmail dot com>
2019-05-09 11:21:17 +0200

    Minor changes in toUTF8dir.ps1

--- misc/toUTF8dir.ps1
+++ misc/toUTF8dir.ps1
@@ -1,11 +1,16 @@
 # Windows Power Shell script to convert RPG sources downloaded from AS400 to UTF8
 # The first parameter is the name of the file to convert
-param( [string] $dirName = $(throw "Please specify a directory name.") )
+param( [string] $dirName )
 
-cd $dirName
+If ([string]::IsNullOrEmpty($dirName)) { $dirName = Read-Host 'Please enter a directory name' }
+If ([string]::IsNullOrEmpty($dirName)) { throw "A directory name is mandatory" }
+
+Set-Location $dirName
 
 Get-ChildItem $dirName |
         Foreach-Object {
             $outfile = "$_.rpgle"
             get-content -Path $_ | out-file $outfile -encoding utf8
         }
+
+pause "Press any key to continue..."
