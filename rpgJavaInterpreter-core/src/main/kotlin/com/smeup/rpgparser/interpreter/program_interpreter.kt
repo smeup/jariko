104f391c2b62eb744d3cc627c2b7f1b0baa437d5
Federico Tomassetti <federico@tomassetti.me>
2019-05-09 15:47:46 +0200

    creating ProgramInterpreter

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/program_interpreter.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/program_interpreter.kt
@@ -2,13 +2,31 @@ package com.smeup.rpgparser.interpreter
 
 import com.smeup.rpgparser.ast.DataWrapUpChoice
 
-class ProgramInterpreter {
+class ProgramInterpreter(val systemInterface: SystemInterface) {
 
     private val dataWrapUpPolicy = HashMap<RpgProgram, DataWrapUpChoice>()
     private val interpreters = HashMap<RpgProgram, InternalInterpreter>()
 
-    fun execute(rpgProgram: RpgProgram) {
-        
+    fun execute(rpgProgram: RpgProgram, initialValues: Map<String, Value>) {
+        val interpreter = interpreters.getOrPut(rpgProgram) {
+            val ii = InternalInterpreter(systemInterface)
+            ii.interpretationContext = object : InterpretationContext {
+                override val name: String
+                    get() = rpgProgram.name
+                override fun setDataWrapUpPolicy(dataWrapUpChoice: DataWrapUpChoice) {
+                    dataWrapUpPolicy[rpgProgram] = dataWrapUpChoice
+                }
+                override fun shouldReinitialize() : Boolean {
+                    return if (rpgProgram in dataWrapUpPolicy) {
+                        dataWrapUpPolicy[rpgProgram]!! == DataWrapUpChoice.LR
+                    } else {
+                        true
+                    }
+                }
+            }
+            ii
+        }
+        interpreter.execute(rpgProgram.cu, initialValues)
     }
 
 }
