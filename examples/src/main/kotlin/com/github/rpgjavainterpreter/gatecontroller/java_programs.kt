be72ad028f12ef9fd40b49dd9aaea669ab9b40ec
Federico Tomassetti <federico@tomassetti.me>
2019-05-10 15:49:29 +0200

    minor

--- examples/src/main/kotlin/com/github/rpgjavainterpreter/gatecontroller/java_programs.kt
+++ examples/src/main/kotlin/com/github/rpgjavainterpreter/gatecontroller/java_programs.kt
@@ -9,7 +9,7 @@ class JD_URL : JvmProgramByReflection() {
 
     fun run(systemInterface: SystemInterface, @Size(10) funz: String, @Size(10) method: String,
             @Size(1000) URL: String) : List<Value> {
-        println("Invoked $funz $method, URL=$URL")
+        systemInterface.display("Invoked $funz $method, URL=$URL")
         return emptyList()
     }
 }
---
+++ examples/src/test/kotlin/GateControllerTest.kt
@@ -0,0 +1,18 @@
+package com.github.rpgjavainterpreter.gatecontroller
+
+import com.smeup.rpgparser.jvminterop.JavaSystemInterface
+import com.smeup.rpgparser.rgpinterop.DirRpgProgramFinder
+import com.smeup.rpgparser.rgpinterop.RpgSystem
+import org.junit.Test
+import java.io.File
+import kotlin.test.assertEquals
+
+class GateControllerTest {
+
+    @Test fun runExample1() {
+        RpgSystem.addProgramFinder(DirRpgProgramFinder(File("rpg")))
+        JavaSystemInterface.addJavaInteropPackage("com.github.rpgjavainterpreter.gatecontroller")
+        JD_001().call("https://xxx.myurl.com", "x", "w")
+        assertEquals(listOf("Invoked  , URL=https://www.myurl.com"), JavaSystemInterface.consoleOutput)
+    }
+}
