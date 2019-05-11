cbb7004925e8ea2deb03d803fe561c24814435e2
Franco Lombardo <f dot lombardo 69 at gmail dot com>
2019-05-11 10:57:30 +0200

    Added small DOS utility for renaming files downloaded from AS400

---
+++ misc/remove_prefix.bat
@@ -0,0 +1,12 @@
+rem Utility for removing a prefix from file names.
+rem We can use it to remove the file name from members downloaded fromm AS400
+rem Usage:
+rem Remove_Prefix.bat  prefix  fileMask
+rem For example, in order to remove the QILEGEN. prefix, use:
+rem remove_prefix.bat QILEGEN. *.*
+
+setlocal
+for %%A in ("%~1%~2") do (
+  set "fname=%%~A"
+  call ren "%%fname%%" "%%fname:*%~1=%%"
+)
