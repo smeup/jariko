4252945392c458cd868ea2d12178eeae98c34431
Franco Lombardo <f dot lombardo 69 at gmail dot com>
2019-05-20 18:02:19 +0200

    Minor fix in test case

--- examples/src/test/kotlin/GateControllerTest.kt
+++ examples/src/test/kotlin/GateControllerTest.kt
@@ -10,7 +10,7 @@ import kotlin.test.assertEquals
 class GateControllerTest {
 
     @Test fun runExample1() {
-        RpgSystem.addProgramFinder(DirRpgProgramFinder(File("rpg")))
+        RpgSystem.addProgramFinder(DirRpgProgramFinder(File("examples/rpg")))
         JavaSystemInterface.addJavaInteropPackage("com.github.rpgjavainterpreter.gatecontroller")
         JD_001().call("https://xxx.myurl.com", "x", "w")
         assertEquals(listOf("Invoked  , URL=https://www.myurl.com"), JavaSystemInterface.consoleOutput)
