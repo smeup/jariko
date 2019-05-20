9d40e92664ab59d6d8e108b7a45d49101f16414e
Franco Lombardo <f dot lombardo 69 at gmail dot com>
2019-05-20 18:39:45 +0200

    Minor fix in tests

--- examples/src/test/kotlin/GateControllerTest.kt
+++ examples/src/test/kotlin/GateControllerTest.kt
@@ -11,6 +11,7 @@ class GateControllerTest {
 
     @Test fun runExample1() {
         RpgSystem.addProgramFinder(DirRpgProgramFinder(File("examples/rpg")))
+        RpgSystem.addProgramFinder(DirRpgProgramFinder(File("rpg")))
         JavaSystemInterface.addJavaInteropPackage("com.github.rpgjavainterpreter.gatecontroller")
         JD_001().call("https://xxx.myurl.com", "x", "w")
         assertEquals(listOf("Invoked  , URL=https://www.myurl.com"), JavaSystemInterface.consoleOutput)
