9721a40f32184f014c88020f22df077021935086
mctaverna <tavernamaurizio@gmail.com>
2019-07-04 17:32:09 +0200

    Added data class MuteComparisonAnnotation

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/ast/mute.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/ast/mute.kt
@@ -4,3 +4,7 @@ import com.strumenta.kolasu.model.Node
 import com.strumenta.kolasu.model.Position
 
 abstract class MuteAnnotation(override val position: Position? = null) : Node(position)
+
+
+
+data class MuteComparisonAnnotation(val val1: Expression, val val2: Expression, val comparison: String, override val position: Position? = null  ) : MuteAnnotation(position)
