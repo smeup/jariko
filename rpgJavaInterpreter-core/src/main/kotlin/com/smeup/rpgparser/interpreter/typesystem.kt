6d43dfd6d8b40f55447952d7eb26293ba5e27727
mctaverna <tavernamaurizio@gmail.com>
2019-09-19 16:44:18 +0200

    Added rpgType to NumberType

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/typesystem.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/typesystem.kt
@@ -54,14 +54,17 @@ data class CharacterType(val nChars: Int) : Type() {
         get() = nChars.toLong()
 }
 
-data class NumberType(val entireDigits: Int, val decimalDigits: Int) : Type() {
+data class NumberType(val entireDigits: Int, val decimalDigits: Int,val rpgType : String = "") : Type() {
     override val size: Long
         get() = (entireDigits + decimalDigits).toLong()
     val integer: Boolean
         get() = decimalDigits == 0
     val decimal: Boolean
         get() = !integer
+
 }
+
+
 data class ArrayType(val element: Type, val nElements: Int, val compileTimeRecordsPerLine: Int? = null) : Type() {
     override val size: Long
         get() = element.size * nElements
@@ -76,6 +79,7 @@ data class ArrayType(val element: Type, val nElements: Int, val compileTimeRecor
 
     fun compileTimeArray(): Boolean = compileTimeRecordsPerLine != null
 }
+
 
 data class FieldType(val name: String, val type: Type)
 
