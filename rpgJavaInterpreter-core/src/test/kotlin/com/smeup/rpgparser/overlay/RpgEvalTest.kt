f4cc7eab32e4bb015bb04caef9ac8272d5098d88
mctaverna <tavernamaurizio@gmail.com>
2019-10-30 15:33:47 +0100

    Review, removed temporary functions

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/overlay/RpgEvalTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/overlay/RpgEvalTest.kt
@@ -1,56 +1,15 @@
 package com.smeup.rpgparser.overlay
 
 import com.smeup.rpgparser.executeAnnotations
-import com.smeup.rpgparser.inputStreamFor
 import com.smeup.rpgparser.interpreter.*
 import org.junit.Test
-
-import kotlin.test.assertTrue
-
 import com.smeup.rpgparser.jvminterop.JavaSystemInterface
-import com.smeup.rpgparser.parsing.ast.CompilationUnit
-import com.smeup.rpgparser.parsing.facade.RpgParserFacade
-import com.smeup.rpgparser.parsing.facade.RpgParserResult
-import com.smeup.rpgparser.parsing.parsetreetoast.ToAstConfiguration
-import com.smeup.rpgparser.parsing.parsetreetoast.injectMuteAnnotation
 import com.smeup.rpgparser.parsing.parsetreetoast.resolve
-import com.smeup.rpgparser.parsing.parsetreetoast.toAst
 import com.smeup.rpgparser.rgpinterop.DirRpgProgramFinder
 import com.smeup.rpgparser.rgpinterop.RpgSystem
 import java.io.File
 
 class RpgEvalTest {
-
-    // Temporary replacement to return RpgParserResult
-    private fun assertCanBeParsed(exampleName: String, withMuteSupport: Boolean = true): RpgParserResult {
-        val result = RpgParserFacade()
-                .apply { this.muteSupport = withMuteSupport }
-                .parse(inputStreamFor(exampleName))
-        assertTrue(result.correct,
-                message = "Errors: ${result.errors.joinToString(separator = ", ")}")
-
-        return result
-    }
-
-    // Temporary replacement
-    private fun assertASTCanBeProduced(
-        exampleName: String,
-        considerPosition: Boolean = false,
-        withMuteSupport: Boolean = true
-    ): CompilationUnit {
-        val parseTreeRoot = assertCanBeParsed(exampleName, withMuteSupport)
-        val ast = parseTreeRoot.root!!.rContext.toAst(ToAstConfiguration(
-                considerPosition = considerPosition))
-        if (withMuteSupport) {
-            if (!considerPosition) {
-                throw IllegalStateException("Mute annotations can be injected only when retaining the position")
-            }
-        }
-        if (withMuteSupport) {
-            ast.injectMuteAnnotation(parseTreeRoot.root!!.muteContexts!!)
-        }
-        return ast
-    }
 
     @Test
     fun EVAL_runtime() {
