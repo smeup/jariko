4e5e31f30c889afd75f526b6736d352f250c1b03
mctaverna <tavernamaurizio@gmail.com>
2019-10-30 16:09:45 +0100

    Review, minor fixes

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/values.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/values.kt
@@ -165,6 +165,7 @@ data class IntValue(val value: Long) : NumberValue, Value() {
 
 data class DecimalValue(val value: BigDecimal) : NumberValue, Value() {
     override fun negate(): Value = DecimalValue(-value)
+
     override fun asInt(): IntValue {
 
         return IntValue(value.toLong())
@@ -178,7 +179,7 @@ data class DecimalValue(val value: BigDecimal) : NumberValue, Value() {
     }
 
     fun isPositive(): Boolean {
-        return value >= BigDecimal.ZERO
+        return value.signum() >= 0
     }
 
     companion object {
@@ -327,7 +328,8 @@ class ProjectedArrayValue(val container: ArrayValue, val field: FieldDefinition)
         require(index <= arrayLength())
         require(value.assignableTo((field.type as ArrayType).element)) { "Assigning to field $field incompatible value $value" }
         val containerElement = container.getElement(index)
-        // TODO to review
+
+        // Set the value within the projected Array
         if (containerElement is StringValue) {
             if (value is StringValue) {
                 containerElement.setSubstring(field.startOffset, field.endOffset, value)
@@ -336,7 +338,7 @@ class ProjectedArrayValue(val container: ArrayValue, val field: FieldDefinition)
                 val pad = s.padStart(field.endOffset - field.startOffset)
                 containerElement.setSubstring(field.startOffset, field.endOffset, StringValue(pad))
             } else {
-                TODO()
+                TODO("$value not supported")
             }
         } else if (containerElement is DataStructValue) {
             if (value is StringValue) {
@@ -346,7 +348,7 @@ class ProjectedArrayValue(val container: ArrayValue, val field: FieldDefinition)
                 val pad = s.padStart(field.endOffset - field.startOffset)
                 containerElement.setSubstring(field.startOffset, field.endOffset, StringValue(pad))
             } else {
-                TODO()
+                TODO("$value not supported")
             }
         }
     }
@@ -358,7 +360,7 @@ class ProjectedArrayValue(val container: ArrayValue, val field: FieldDefinition)
         } else if (containerElement is DataStructValue) {
             return containerElement.getSubstring(field.startOffset, field.endOffset)
         } else {
-            TODO()
+            TODO("$containerElement not supported")
         }
     }
 }
@@ -400,7 +402,8 @@ fun Type.blank(): Value {
 data class DataStructValue(var value: String) : Value() {
     override fun assignableTo(expectedType: Type): Boolean {
         return when (expectedType) {
-            is DataStructureType -> expectedType.elementSize == value.length // Check for >= ???
+            // Check if the size of the value mathches the expected size within the DS
+            is DataStructureType -> expectedType.elementSize == value.length
             is StringType -> expectedType.size == this.value.length.toLong()
             else -> false
         }
