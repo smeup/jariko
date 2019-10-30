335ccdc4d98e60ce5ab225ce3363fb2903d304c2
mctaverna <tavernamaurizio@gmail.com>
2019-10-30 15:35:53 +0100

    Review, removed temporary functions

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/overlay/RpgParserOverlayTest12.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/overlay/RpgParserOverlayTest12.kt
@@ -1,54 +1,18 @@
 package com.smeup.rpgparser.overlay
 
+import com.smeup.rpgparser.assertASTCanBeProduced
+import com.smeup.rpgparser.assertCanBeParsed
 import com.smeup.rpgparser.executeAnnotations
-import com.smeup.rpgparser.inputStreamFor
 import com.smeup.rpgparser.interpreter.ArrayType
 import com.smeup.rpgparser.interpreter.CharacterType
 import com.smeup.rpgparser.interpreter.InternalInterpreter
 import com.smeup.rpgparser.jvminterop.JavaSystemInterface
-import com.smeup.rpgparser.parsing.ast.CompilationUnit
-import com.smeup.rpgparser.parsing.facade.RpgParserFacade
-import com.smeup.rpgparser.parsing.facade.RpgParserResult
-import com.smeup.rpgparser.parsing.parsetreetoast.ToAstConfiguration
-import com.smeup.rpgparser.parsing.parsetreetoast.injectMuteAnnotation
 import com.smeup.rpgparser.parsing.parsetreetoast.resolve
-import com.smeup.rpgparser.parsing.parsetreetoast.toAst
 import org.junit.Ignore
 import org.junit.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
 class RpgParserOverlayTest12 {
-
-    // Temporary replacement to return RpgParserResult
-    private fun assertCanBeParsed(exampleName: String, withMuteSupport: Boolean = true): RpgParserResult {
-        val result = RpgParserFacade()
-                .apply { this.muteSupport = withMuteSupport }
-                .parse(inputStreamFor(exampleName))
-        assertTrue(result.correct,
-                message = "Errors: ${result.errors.joinToString(separator = ", ")}")
-
-        return result
-    }
-    // Temporary replacement
-    private fun assertASTCanBeProduced(
-        exampleName: String,
-        considerPosition: Boolean = false,
-        withMuteSupport: Boolean = true
-    ): CompilationUnit {
-        val parseTreeRoot = assertCanBeParsed(exampleName, withMuteSupport)
-        val ast = parseTreeRoot.root!!.rContext.toAst(ToAstConfiguration(
-                considerPosition = considerPosition))
-        if (withMuteSupport) {
-            if (!considerPosition) {
-                throw IllegalStateException("Mute annotations can be injected only when retaining the position")
-            }
-        }
-        if (withMuteSupport) {
-            ast.injectMuteAnnotation(parseTreeRoot.root!!.muteContexts!!)
-        }
-        return ast
-    }
 
     @Test
     fun parseMUTE12_01_syntax() {
