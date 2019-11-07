4c87facbfc2f8fa212b1e34ff5b68285e91a0183
Federico Tomassetti <federico@tomassetti.me>
2019-11-07 17:59:35 +0100

    explain shadow behavior of params in plist #issue33

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/parsing/ast/statements.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/parsing/ast/statements.kt
@@ -210,6 +210,8 @@ data class PlistStmt(
 ) : Statement(position), StatementThatCanDefineData {
     override fun dataDefinition(): List<InStatementDataDefinition> {
         val allDataDefinitions = params.mapNotNull { it.dataDefinition }
+        // We do not want params in plist to shadow existing data definitions
+        // They are implicit data definitions only when explicit data definitions are not present
         val filtered = allDataDefinitions.filter {paramDataDef ->
             val containingCU = this.ancestor(CompilationUnit::class.java) ?: throw IllegalStateException("Not contained in a CU")
             containingCU!!.dataDefinitions.none { it.name == paramDataDef.name}
