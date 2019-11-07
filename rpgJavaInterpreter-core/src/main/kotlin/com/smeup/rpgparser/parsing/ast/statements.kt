bb0f6bbe4950dbbfa8084fffc5f8ee9c79c421f4
Federico Tomassetti <federico@tomassetti.me>
2019-11-07 18:02:51 +0100

    minor

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/parsing/ast/statements.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/parsing/ast/statements.kt
@@ -214,7 +214,7 @@ data class PlistStmt(
         // They are implicit data definitions only when explicit data definitions are not present
         val filtered = allDataDefinitions.filter { paramDataDef ->
             val containingCU = this.ancestor(CompilationUnit::class.java) ?: throw IllegalStateException("Not contained in a CU")
-            containingCU!!.dataDefinitions.none { it.name == paramDataDef.name }
+            containingCU.dataDefinitions.none { it.name == paramDataDef.name }
         }
         return filtered
     }
