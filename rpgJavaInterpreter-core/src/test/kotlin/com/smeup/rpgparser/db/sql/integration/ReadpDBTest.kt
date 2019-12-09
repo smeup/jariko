606a1c632963fdc97f7d973f5193cb613fcba2af
Franco Lombardo <f.lombardo69@gmail.com>
2019-12-09 15:37:45 +0100

    First test for READP

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/db/sql/integration/ReadpDBTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/db/sql/integration/ReadpDBTest.kt
@@ -2,18 +2,19 @@ package com.smeup.rpgparser.db.sql.integration
 
 import com.smeup.rpgparser.db.sql.outputOfDBPgm
 import org.junit.Test
-import kotlin.test.Ignore
 import kotlin.test.assertEquals
 
-class ReadDBTest {
+class ReadpDBTest {
 
-    @Test @Ignore
+    @Test
     fun findsExistingRecords() {
         val outputLines = outputOfDBPgm(
-            "db/SETLLREADP",
+            "db/READ01",
             listOf(createEMPLOYEE(), createXEMP2(), createXEMP2Index(), insertRecords()),
             emptyMap()
         )
-        assertEquals(listOf("200340", "200330", "200310", "200280", "200240"), outputLines)
+        assertEquals("000010", outputLines.first())
+        assertEquals("200340", outputLines.last())
+        assertEquals(42, outputLines.size)
     }
 }
