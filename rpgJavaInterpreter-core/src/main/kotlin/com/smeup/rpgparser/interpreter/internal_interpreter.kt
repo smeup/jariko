874d2996a9ffdbe3ef1f2ff931cb5a81355d54c5
Franco Lombardo <f.lombardo69@gmail.com>
2020-03-12 12:53:58 +0100

    Solved NPE accessing indicators

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/internal_interpreter.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/internal_interpreter.kt
@@ -386,25 +386,25 @@ class InternalInterpreter(val systemInterface: SystemInterface) : InterpreterCor
     private fun Statement.solveIndicatorValues(): MutableList<SolvedIndicatorCondition> {
         val indicatorValues: MutableList<SolvedIndicatorCondition> = mutableListOf()
         var i = 0
-        this.continuedIndicators.forEach { (indicator, continuedIndicator) ->
+        this.continuedIndicators.forEach { (indicatorKey, continuedIndicator) ->
             var condition: Boolean = false
 
             // continuedIndicator operator is N (negate) and predefinedIndicator is false, condition verified!
-            if (continuedIndicator.negate && !predefinedIndicators[indicator]!!.value) condition = true
+            if (continuedIndicator.negate && !indicator(indicatorKey).value) condition = true
 
             // continuedIndicator operator is " " (not negate) and predefinedIndicator is true, condition verified!
-            if (!continuedIndicator.negate && predefinedIndicators[indicator]!!.value) condition = true
+            if (!continuedIndicator.negate && indicator(indicatorKey).value) condition = true
 
             // continuedIndicator operator is N (negate) and predefinedIndicator is true, condition verified!
-            if (continuedIndicator.negate && predefinedIndicators[indicator]!!.value) condition = false
+            if (continuedIndicator.negate && indicator(indicatorKey).value) condition = false
 
             // continuedIndicator operator is " " (not negate) and predefinedIndicator is false, condition verified!
-            if (!continuedIndicator.negate && predefinedIndicators[indicator]!!.value) condition = false
+            if (!continuedIndicator.negate && indicator(indicatorKey).value) condition = false
 
-            val solvedIndicatorCondition = SolvedIndicatorCondition(indicator, condition, continuedIndicator.level)
+            val solvedIndicatorCondition = SolvedIndicatorCondition(indicatorKey, condition, continuedIndicator.level)
             indicatorValues.add(i, solvedIndicatorCondition)
             i++
-            println("Indicator:${continuedIndicator.key}(negate=${continuedIndicator.negate}, operator=${solvedIndicatorCondition.operator}) PredefinedValue(from Symboltable)=${predefinedIndicators[indicator]} --> Is condition verified: ${solvedIndicatorCondition.value}")
+            println("Indicator:${continuedIndicator.key}(negate=${continuedIndicator.negate}, operator=${solvedIndicatorCondition.operator}) PredefinedValue(from Symboltable)=${indicator(indicatorKey)} --> Is condition verified: ${solvedIndicatorCondition.value}")
         }
         return indicatorValues
     }
