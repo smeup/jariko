fa3548a4be7634dd6eb2aea6a38ce725e0330a3c
Franco Parodi <franco.parodi@smeup.com>
2020-04-24 11:44:27 +0200

    Fix MUTEExamplesTest.kt due to Kute10_56

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/evaluation/MUTEExamplesTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/evaluation/MUTEExamplesTest.kt
@@ -3,7 +3,7 @@ package com.smeup.rpgparser.evaluation
 import Kute10_53
 import Kute10_54
 import Kute10_55
-import Kute10_56_
+import Kute10_56
 import com.andreapivetta.kolor.yellow
 import com.smeup.rpgparser.CollectorSystemInterface
 import com.smeup.rpgparser.DummyProgramFinder
@@ -367,7 +367,7 @@ class MUTEExamplesTest {
     @Test @Category(PerformanceTest::class)
     // DOU (loop of 10000000 iterations with increment of a numeric var)
     fun executeKUTE10_56() {
-        val kute10_56 = Kute10_56_()
+        val kute10_56 = Kute10_56()
         kute10_56.performanceComparing()
     }
 
