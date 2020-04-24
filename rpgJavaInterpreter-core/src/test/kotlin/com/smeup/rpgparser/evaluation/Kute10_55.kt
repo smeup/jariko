c821807c1ae2a3ccd3624fba538c40745de4a7f8
Franco Parodi <franco.parodi@smeup.com>
2020-04-24 10:44:42 +0200

    Code comments

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/evaluation/Kute10_55.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/evaluation/Kute10_55.kt
@@ -1,48 +1,78 @@
-import com.smeup.rpgparser.PerformanceTest
 import com.smeup.rpgparser.interpreter.*
 import com.smeup.rpgparser.parsing.ast.*
 import com.smeup.rpgparser.parsing.ast.AssignmentOperator.*
 import com.strumenta.kolasu.model.ReferenceByName
 import org.junit.Test
-import org.junit.experimental.categories.Category
 import java.nio.charset.Charset
-import kotlin.reflect.jvm.internal.impl.resolve.constants.LongValue
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
+
+/**
+    Kute10_55.kt is the "hybrid" way to perform the similar MUTE10_55.rpgle DOW (do-while) loop code.
+
+    - (MUTE10_55.rpgle)
+       C                   DOW       $X<10000001
+       C                   EVAL      $X=$X+1
+       C                   ENDDO
+
+    - (Kute10_55.kt)
+       while (eval(lessThanExpr).asBoolean().value) {
+           execute(evalStatement)
+           loopCounter++
+       }
+
+    "Hybrid" means it uses both Kotlin technology and the Jariko intepreter implementation, so, for exemple
+    the increment of the variable checked for each iteration is done using "EvalStmt" (not simply the plus "+" operator)
+    and the value of the variable is stored (read/write) in globalSymbolTable (SymbolTable) Map.
+    This approach should be useful for developer to be more efficient to develop/debug code due to
+    performance tuning, in a more concise developing environment.
+
+ */
 
 class Kute10_55 {
 
     private val charset = Charset.forName("Cp037")
     private var globalSymbolTable = SymbolTable()
-    private var actualElapsedTimeInMillisec = 0L
     private val expectedElapsedTimeInMillisec = 83L
     private var loopCounter = 0L
 
     @Test
     public fun performanceComparing() {
 
+        // The variable name, 'X' instead of '$X' due to Kotlin syntax
         val name = "X"
         val type = NumberType(10, 0, "I")
         var referred: DataDefinition = DataDefinition(name, type)
         var variable: ReferenceByName<AbstractDataDefinition> = ReferenceByName("X", referred)
         var dataRefExpr = DataRefExpr(variable)
+
+        // Store the variable
         globalSymbolTable[referred] = IntValue.ZERO.asInt()
+
+        // Create the EvalStmt (similar to 'toAst' does)
         val rightInt = IntLiteral(10000000)
         val lessThanExpr = LessThanExpr(dataRefExpr, rightInt)
         val intLiteral = IntLiteral(1)
         val plusExpr = PlusExpr(dataRefExpr, intLiteral)
         var evalStatement = dummyEvalStmt(dataRefExpr, plusExpr, NORMAL_ASSIGNMENT)
 
-        val startTime = System.currentTimeMillis()
-        while (eval(lessThanExpr).asBoolean().value) {
-            execute(evalStatement)
-            loopCounter++
+        val actualElapsedTimeInMillisec = measureTimeMillis {
+            // 'DOW' implementation
+            doWhile(lessThanExpr, evalStatement)
         }
-        actualElapsedTimeInMillisec = System.currentTimeMillis() - startTime
+
+        // Results
         var message = "Expected execution of ${rightInt.value} iterations ($loopCounter done) takes less or same to $expectedElapsedTimeInMillisec ms. Actual is $actualElapsedTimeInMillisec ms."
         assertTrue(actualElapsedTimeInMillisec <= expectedElapsedTimeInMillisec, message)
         message = "Expected execution of ${rightInt.value} iterations, actual is $loopCounter iterations."
         assertEquals(rightInt.value, loopCounter, message)
+    }
+
+    private fun doWhile(lessThanExpr: Expression, evalStatement: EvalStmt) {
+        while (eval(lessThanExpr).asBoolean().value) {
+            execute(evalStatement)
+            loopCounter++
+        }
     }
 
     private fun execute(statement: EvalStmt) {
@@ -53,6 +83,7 @@ class Kute10_55 {
             assign(statement.target, statement.expression, statement.operator)
         }
     }
+
     private fun condition(loopCounter: Long, endLimit: Long): BooleanValue {
         return BooleanValue(loopCounter <= endLimit)
     }
@@ -118,6 +149,7 @@ class Kute10_55 {
     }
 
     private fun interpretConcrete(expression: Expression): Value {
+        // PAY ATTENTION: only a reduced set of expressions is available
         return when (expression) {
             is StringLiteral -> StringValue(expression.value)
             is IntLiteral -> IntValue(expression.value)
@@ -224,5 +256,11 @@ class Kute10_55 {
                 plusAssignmentOperator,
                 flags = flags,
                 position = null)
+    }
+
+    private inline fun measureTimeMillis(block: () -> Unit): Long {
+        val start = System.currentTimeMillis()
+        block()
+        return System.currentTimeMillis() - start
     }
 }
