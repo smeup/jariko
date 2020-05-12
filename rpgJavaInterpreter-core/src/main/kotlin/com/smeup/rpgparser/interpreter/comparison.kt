c87d107f4a577d4e23660cf0767a4cf0889f25a7
Franco Lombardo <f.lombardo69@gmail.com>
2020-05-12 19:03:39 +0200

    Comparison is now a typealias

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/comparison.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/comparison.kt
@@ -4,37 +4,37 @@ import com.smeup.rpgparser.parsing.ast.Expression
 import com.smeup.rpgparser.utils.ComparisonOperator
 import java.nio.charset.Charset
 
-enum class Comparison {
-    SMALLER,
-    EQUAL,
-    GREATER
-}
+typealias Comparison = Int
+
+const val EQUAL = 0
+const val SMALLER = -1
+const val GREATER = 1
 
 fun isEqualOrSmaller(value1: Value, value2: Value, charset: Charset): Boolean {
     val cmp = compare(value1, value2, charset)
-    return cmp == Comparison.SMALLER || cmp == Comparison.EQUAL
+    return cmp == SMALLER || cmp == EQUAL
 }
 
 fun isEqualOrGreater(value1: Value, value2: Value, charset: Charset): Boolean {
     val cmp = compare(value1, value2, charset)
-    return cmp == Comparison.GREATER || cmp == Comparison.EQUAL
+    return cmp == GREATER || cmp == EQUAL
 }
 
 fun isGreaterThan(value1: Value, value2: Value, charset: Charset): Boolean {
     val cmp = compare(value1, value2, charset)
-    return cmp == Comparison.GREATER
+    return cmp == GREATER
 }
 
 fun isSmallerThan(value1: Value, value2: Value, charset: Charset): Boolean {
     val cmp = compare(value1, value2, charset)
-    return cmp == Comparison.SMALLER
+    return cmp == SMALLER
 }
 
 fun compare(value1: Value, value2: Value, charset: Charset): Comparison {
     return when {
         value1 is IntValue && value2 is IntValue -> intComparison(value1, value2)
         value1 is IntValue && value2 is StringValue -> throw RuntimeException("Cannot compare int and string")
-        value2 is HiValValue -> Comparison.SMALLER
+        value2 is HiValValue -> SMALLER
         value1 is NumberValue && value2 is NumberValue -> numberComparison(value1, value2)
         value1 is StringValue && value2 is StringValue -> stringComparison(value1, value2, charset)
         else -> TODO("Unable to compare: value 1 is $value1, Value 2 is $value2")
@@ -43,26 +43,26 @@ fun compare(value1: Value, value2: Value, charset: Charset): Comparison {
 
 private fun numberComparison(value1: NumberValue, value2: NumberValue): Comparison =
     when {
-        value1.bigDecimal == value2.bigDecimal -> Comparison.EQUAL
-        value1.bigDecimal < value2.bigDecimal -> Comparison.SMALLER
-        else -> Comparison.GREATER
+        value1.bigDecimal == value2.bigDecimal -> EQUAL
+        value1.bigDecimal < value2.bigDecimal -> SMALLER
+        else -> GREATER
     }
 
 private fun intComparison(value1: IntValue, value2: IntValue): Comparison =
     when {
-        value1.value == value2.value -> Comparison.EQUAL
-        value1.value < value2.value -> Comparison.SMALLER
-        else -> Comparison.GREATER
+        value1.value == value2.value -> EQUAL
+        value1.value < value2.value -> SMALLER
+        else -> GREATER
     }
 
 fun stringComparison(value1: Value, value2: Value, charset: Charset): Comparison {
     if (value1 == value2) {
-        return Comparison.EQUAL
+        return EQUAL
     }
     if (value1.compare(value2, charset) < 0) {
-        return Comparison.SMALLER
+        return SMALLER
     }
-    return Comparison.GREATER
+    return GREATER
 }
 
 data class ComparisonResult(val isVerified: Boolean, val comparison: Comparison)
@@ -70,9 +70,9 @@ data class ComparisonResult(val isVerified: Boolean, val comparison: Comparison)
 fun ComparisonOperator?.verify(factor1: Expression, factor2: Expression, interpreter: InterpreterCore, charset: Charset): ComparisonResult {
     val comparison = interpreter.compareExpressions(factor1, factor2, charset)
     return when (comparison) {
-        Comparison.EQUAL -> ComparisonResult(this == null || this == ComparisonOperator.EQ || this == ComparisonOperator.GE || this == ComparisonOperator.LE, comparison)
-        Comparison.SMALLER -> ComparisonResult(this == null || this == ComparisonOperator.LE || this == ComparisonOperator.LT, comparison)
-        Comparison.GREATER -> ComparisonResult(this == null || this == ComparisonOperator.GE || this == ComparisonOperator.GT, comparison)
+        EQUAL -> ComparisonResult(this == null || this == ComparisonOperator.EQ || this == ComparisonOperator.GE || this == ComparisonOperator.LE, comparison)
+        SMALLER -> ComparisonResult(this == null || this == ComparisonOperator.LE || this == ComparisonOperator.LT, comparison)
+        else -> ComparisonResult(this == null || this == ComparisonOperator.GE || this == ComparisonOperator.GT, comparison)
     }
 }
 
