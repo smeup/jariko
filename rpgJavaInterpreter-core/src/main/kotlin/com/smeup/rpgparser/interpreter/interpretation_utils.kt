334b97004bd79f7b2f22f1e57d4f2f3fae1a9dcd
Franco Lombardo <f.lombardo69@gmail.com>
2020-09-04 15:11:47 +0200

    Minor refactoring

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/interpretation_utils.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/interpretation_utils.kt
@@ -8,17 +8,18 @@ fun Value.stringRepresentation(format: String? = null): String {
         is BooleanValue -> asString().value // TODO check if it's the best solution
         is NumberValue -> render()
         is ArrayValue -> "[${elements().map { it.render() }.joinToString(", ")}]"
-        is TimeStampValue -> {
-            // TODO this is a simple stub for what will be the full implementation
-            if ("*ISO0" == format) {
-                SimpleDateFormat("yyyyMMddHHmmssSSS000").format(value)
-            } else {
-                SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX").format(value)
-            }
-        }
+        is TimeStampValue -> timestampFormatting(format)
         is DataStructValue -> value.trimEnd()
         is ZeroValue -> "0"
         is AllValue -> charsToRepeat
         else -> TODO("Unable to render value $this (${this.javaClass.canonicalName})")
     }
 }
+
+private fun TimeStampValue.timestampFormatting(format: String?): String =
+    // TODO this is a simple stub for what will be the full implementation
+    if ("*ISO0" == format) {
+        SimpleDateFormat("yyyyMMddHHmmssSSS000").format(value)
+    } else {
+        SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX").format(value)
+    }
