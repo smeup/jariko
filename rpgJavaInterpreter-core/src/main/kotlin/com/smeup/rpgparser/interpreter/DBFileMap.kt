de414d4b5cac37792ecfab27023298109dd7f3be
Marco Lanari <marco.lanari@smeup.com>
2021-02-24 10:28:28 +0100

    Fixed compilation issue due to the DBFile interface signature modification in Reload master snapshot

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/DBFileMap.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/DBFileMap.kt
@@ -52,6 +52,8 @@ data class EnrichedDBFile(private val dbFile: DBFile, private val fileDefinition
 
     override var name = dbFile.name
 
+    override var logger = dbFile.logger
+
     override fun chain(key: String) = dbFile.chain(key).validate()
 
     override fun chain(keys: List<String>) = dbFile.chain(keys).validate()
