f865f3106e5105e078e38e079d07d0029c2bece1
franco.parodi <franco.parodi@smeup.com>
2021-09-02 10:36:02 +0200

    Remove static vars, handle exceptions due to 'request changes'

--- rpgJavaInterpreter-core/src/main/java/com/smeup/rpgparser/utils/EBCDICComparator.java
+++ rpgJavaInterpreter-core/src/main/java/com/smeup/rpgparser/utils/EBCDICComparator.java
@@ -7,31 +7,34 @@ import java.util.Comparator;
 
 public class EBCDICComparator implements Comparator<String> {
 
-    private final static String EBCDIC_CODE = "CP037";
-    private final static Charset STANDARD_CHARSET = StandardCharsets.ISO_8859_1;
-    private static byte[] b1;
-    private static byte[] b2;
+    private final String EBCDIC_CODE = "CP037";
+    private final Charset STANDARD_CHARSET = StandardCharsets.ISO_8859_1;
     private int order = 1;
 
-    public EBCDICComparator() { }
+    public EBCDICComparator() {
+    }
 
-    public EBCDICComparator(boolean reverseOrder) {
-        if(reverseOrder) order = -1;
+    public EBCDICComparator(boolean descend) {
+        if (descend)
+            order = -1;
     }
 
     @Override
     public int compare(String s1, String s2) {
 
-        if (s1.equals(s2)) return 0;
+        if (s1.equals(s2))
+            return 0;
 
+        byte[] b1;
+        byte[] b2;
         try {
             b1 = s1.getBytes(EBCDIC_CODE);
             b2 = s2.getBytes(EBCDIC_CODE);
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
 
-        return order * (new String(b1, STANDARD_CHARSET).compareTo(new String(b2, STANDARD_CHARSET)));
+        return order * new String(b1, STANDARD_CHARSET).compareTo(new String(b2, STANDARD_CHARSET));
     }
 }
 
