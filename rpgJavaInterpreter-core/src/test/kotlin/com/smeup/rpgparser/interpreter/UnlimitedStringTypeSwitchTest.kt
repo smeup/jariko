47456d5c76cde38da798515941d2340116fbf860
lanarimarco <marco.lanari@smeup.com>
2023-06-15 14:40:34 +0200

    Added some tests that load rpg sources with zone types and depending on UnlimitedStringTypeSwitch the DataDefinition.type is a StringType rather than UnlimitedStringType instance

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/interpreter/UnlimitedStringTypeSwitchTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/interpreter/UnlimitedStringTypeSwitchTest.kt
@@ -16,29 +16,88 @@
 
 package com.smeup.rpgparser.interpreter
 
+import com.smeup.rpgparser.AbstractTest
 import org.junit.After
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
-class UnlimitedStringTypeSwitchTest {
+class UnlimitedStringTypeSwitchTest : AbstractTest() {
 
     private val featuresFactory = FeaturesFactory.newInstance()
 
+    /**
+     * Assert that if UnlimitedStringTypeSwitch is default featuresFactory.createStringType returns
+     * an instance of StringType
+     * */
     @Test
-    fun createDefaultStringType() {
+    fun createStringType() {
         val type = featuresFactory.createStringType { StringType(10, false) }
         assertTrue(type is StringType)
     }
 
+    /**
+     * Assert that if UnlimitedStringTypeSwitch is on featuresFactory.createStringType returns
+     * an instance of UnlimitedStringType
+     * */
     @Test
     fun createUnlimitedStringType() {
-        System.setProperty(FeatureFlag.UnlimitedStringTypeSwitch.getPropertyName(), "true")
+        switchOn()
         val type = featuresFactory.createStringType { StringType(10, false) }
         assertTrue(type is UnlimitedStringType)
     }
 
+    /**
+     * Assert that if UnlimitedStringTypeSwitch is default the Msg type is StringType
+     * */
+    @Test
+    fun msgInDSpecIsStringType() {
+        assertASTCanBeProduced("HELLO").apply {
+            assertTrue(getDataDefinition("Msg").type is StringType)
+        }
+    }
+
+    /**
+     * Assert that if UnlimitedStringTypeSwitch is on the Msg type is UnlimitedStringTupe
+     * */
+    @Test
+    fun msgInDSpecIsUnlimitedStringType() {
+        switchOn()
+        assertASTCanBeProduced("HELLO").apply {
+            assertTrue(getDataDefinition("Msg").type is UnlimitedStringType)
+        }
+    }
+
+    /**
+     * Assert that if UnlimitedStringTypeSwitch is default the Msg1 type is StringType
+     * */
+    @Test
+    fun msg1InDSFieldIsStringType() {
+        assertASTCanBeProduced("UNLIMIT_DS").apply {
+            assertTrue(getDataOrFieldDefinition("Msg1").type is StringType)
+        }
+    }
+
+    /**
+     * Assert that if UnlimitedStringTypeSwitch is on the Msg1 type is UnlimitedStringTupe
+     * */
+    @Test
+    fun msg1InDSFieldIsIsUnlimitedStringType() {
+        switchOn()
+        assertASTCanBeProduced("UNLIMIT_DS").apply {
+            assertTrue(getDataOrFieldDefinition("Msg1").type is UnlimitedStringType)
+        }
+    }
+
     @After
     fun tearDown() {
-        System.setProperty(FeatureFlag.UnlimitedStringTypeSwitch.getPropertyName(), "")
+        switchOff()
+    }
+
+    private fun switchOn() {
+        System.setProperty(FeatureFlag.UnlimitedStringTypeSwitch.getPropertyName(), "1")
+    }
+
+    private fun switchOff() {
+        System.setProperty(FeatureFlag.UnlimitedStringTypeSwitch.getPropertyName(), "0")
     }
 }
