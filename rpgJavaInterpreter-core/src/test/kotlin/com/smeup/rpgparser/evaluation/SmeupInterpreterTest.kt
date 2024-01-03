64a5e8e0b466b4557eaa4b61d9e5b2a852074ddf
cosentino-smeup <32836304+cosentino-smeup@users.noreply.github.com>
2024-01-03 13:04:54 +0000

    remove comment

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/evaluation/SmeupInterpreterTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/evaluation/SmeupInterpreterTest.kt
@@ -76,7 +76,6 @@ open class SmeupInterpreterTest : AbstractTest() {
 
     @Test
     fun executeT10_A90() {
-        //val expected = listOf("999-9999 A90_A4(        ) A90_A5(RPG DEPT)", "A90_A4(        ) A90_A5(RPG DEPT)")
         val expected = listOf("A90_A4(        ) A90_A5(RPG DEPT)", "A90_A4(        ) A90_A5(RPG DEPT)")
         assertEquals(expected, "smeup/T10_A90".outputOf())
     }
