746753e72e5b490918895bcfd6318e1610d09e8f
Davide Palladino <davide.palladino@apuliasoft.com>
2024-02-12 10:53:42 +0100

    Improved logic for `GE` into `DOWxxStmt` class

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/parsing/ast/statements.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/parsing/ast/statements.kt
@@ -1258,7 +1258,15 @@ data class DOWxxStmt(
                 }
                 return evaluationFactor1 > evaluationFactor2
             }
-            ComparisonOperator.GE -> evaluationFactor1 >= evaluationFactor2
+            ComparisonOperator.GE -> {
+                if (
+                    (evaluationFactor1 is UnlimitedStringValue || evaluationFactor1 is StringValue) &&
+                    (evaluationFactor2 is UnlimitedStringValue || evaluationFactor2 is StringValue)
+                ) {
+                    return compare(evaluationFactor1, evaluationFactor2, interpreter.getLocalizationContext().charset).run { this == GREATER || this == EQUAL }
+                }
+                return evaluationFactor1 >= evaluationFactor2
+            }
             ComparisonOperator.LT -> evaluationFactor1 < evaluationFactor2
             ComparisonOperator.LE -> evaluationFactor1 <= evaluationFactor2
         }
