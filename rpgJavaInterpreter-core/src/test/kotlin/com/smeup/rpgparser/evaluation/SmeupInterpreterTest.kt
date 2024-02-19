590562135e0c8e931100d1f1461d0d00b22053a4
cosentino-smeup <32836304+cosentino-smeup@users.noreply.github.com>
2024-02-19 10:07:25 +0000

    add test case T02_A50_P05

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/evaluation/SmeupInterpreterTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/evaluation/SmeupInterpreterTest.kt
@@ -382,4 +382,10 @@ open class SmeupInterpreterTest : AbstractTest() {
         val expected = listOf<String>("Res(-A)=-10 Res( -A)= -10")
         assertEquals(expected, "smeup/T02_A60_P03".outputOf())
     }
+
+    @Test
+    fun executeT02_A50_P5() {
+        val expected = listOf<String>("£C5")
+        assertEquals(expected, "smeup/T02_A50_P5".outputOf(configuration = smeupConfig))
+    }
 }
---
+++ rpgJavaInterpreter-core/src/test/resources/smeup/T02_A50_P5.rpgle
@@ -0,0 +1,10 @@
+     D £DBG_Str        S             10
+     DCQNCOG         E DS                  EXTNAME(CQNCOG0F) INZ
+      *
+     DK§CF             DS
+     D K_N§TINC                            LIKE(N§TINC)
+      *
+     C                   EVAL      K_N§TINC='£C5'
+     C     £DBG_Str      DSPLY
+      *
+     C                   SETON                                          LR
