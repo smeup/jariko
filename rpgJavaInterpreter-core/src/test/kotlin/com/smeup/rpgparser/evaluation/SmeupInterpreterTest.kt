c4d012487a5f6dbcb6841fa1a8002431999c8c95
Davide Palladino <davide.palladino@apuliasoft.com>
2024-03-04 10:10:53 +0100

    Implemented pass 09.

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/evaluation/SmeupInterpreterTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/evaluation/SmeupInterpreterTest.kt
@@ -470,4 +470,12 @@ open class SmeupInterpreterTest : AbstractTest() {
         )
         assertEquals(expected, "smeup/T02_A40_P08".outputOf())
     }
+
+    @Test
+    fun executeT02_A40_P09() {
+        val expected = listOf(
+            "CNCLI       AAAAAA         333"
+        )
+        assertEquals(expected, "smeup/T02_A40_P09".outputOf())
+    }
 }
