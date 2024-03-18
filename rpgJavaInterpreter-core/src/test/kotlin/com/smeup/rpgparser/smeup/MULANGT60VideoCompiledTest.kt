d880bf118e591c3304976b148a4ba96996c72095
lanarimarco <marco.lanari@smeup.com>
2024-03-18 14:39:38 +0100

    Fix typo

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT60VideoCompiledTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT60VideoCompiledTest.kt
@@ -1,7 +1,7 @@
 package com.smeup.rpgparser.smeup
 
 /**
- * This class doest must be implemented because it exists just to test the compiled version of the programs defined in the superclass.
+ * This class doesn't must be implemented because it exists just to test the compiled version of the programs defined in the superclass.
  */
 class MULANGT60VideoCompiledTest : MULANGT60VideoTest() {
 
