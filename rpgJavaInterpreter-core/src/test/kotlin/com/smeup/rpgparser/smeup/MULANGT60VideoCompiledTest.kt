abad6d4961f0cca1303b1f469323bd6473602ed7
lanarimarco <marco.lanari@smeup.com>
2024-03-18 14:26:53 +0100

    Add MULANGT60VideoTest

---
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT60VideoCompiledTest.kt
@@ -0,0 +1,9 @@
+package com.smeup.rpgparser.smeup
+
+/**
+ * This class doest must be implemented because it exists just to test the compiled version of the programs defined in the superclass.
+ */
+class MULANGT60VideoCompiledTest : MULANGT60VideoTest() {
+
+    override fun useCompiledVersion() = true
+}
