2a6d6a134687824ee311863efda27c552c408216
lanarimarco <marco.lanari@smeup.com>
2024-03-18 15:29:11 +0100

    Add MULANGT20PerformanceTest scaffold

---
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT20PerformanceTest.kt
@@ -0,0 +1,6 @@
+package com.smeup.rpgparser.smeup
+
+/**
+ * Every method in this class must be annotated with @Test @Category(PerformanceTest::class) annotation
+ * */
+open class MULANGT20PerformanceTest : MULANGTTest()
