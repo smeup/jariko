20a4425030f3bfc3cdba8935617cb5789dd51c99
lanarimarco <marco.lanari@smeup.com>
2024-03-18 15:33:53 +0100

    Add MULANGT50FileAccessTest scaffold

---
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT50FileAccessTest.kt
@@ -0,0 +1,3 @@
+package com.smeup.rpgparser.smeup
+
+open class MULANGT50FileAccessTest : MULANGTTest()
---
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT50FileAccessTestCompiled.kt
@@ -0,0 +1,5 @@
+package com.smeup.rpgparser.smeup
+
+class MULANGT50FileAccessTestCompiled : MULANGT50FileAccessTest() {
+    override fun useCompiledVersion() = true
+}
