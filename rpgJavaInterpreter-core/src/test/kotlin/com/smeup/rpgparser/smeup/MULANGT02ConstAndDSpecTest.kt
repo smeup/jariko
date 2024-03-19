661221a459107a2d39ce776867c642bc6868ca30
Davide Palladino <davide.palladino@apuliasoft.com>
2024-03-19 08:10:17 +0100

    Moved test into opportune file

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT02ConstAndDSpecTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT02ConstAndDSpecTest.kt
@@ -1,3 +1,16 @@
 package com.smeup.rpgparser.smeup
 
-open class MULANGT02ConstAndDSpecTest : MULANGTTest()
+import org.junit.Test
+import kotlin.test.assertEquals
+
+open class MULANGT02ConstAndDSpecTest : MULANGTTest() {
+    /**
+     * I cannot calculate the size of DS5_FL1 from the overlaying fields as there are none
+     * @see #24
+     */
+    @Test
+    fun executeT02_A40_P05() {
+        val expected = listOf("333,zz")
+        assertEquals(expected, "smeup/T02_A40_P05".outputOf())
+    }
+}
