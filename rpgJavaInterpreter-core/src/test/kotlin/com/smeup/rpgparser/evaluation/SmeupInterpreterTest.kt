6b0396cdb7e7d86cc2222e43a032e4900e9536b2
Domenico <domenico.mancini@apuliasoft.com>
2024-03-19 09:00:51 +0100

    Move tests to MULANGT10BaseCodopTest

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/evaluation/SmeupInterpreterTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/evaluation/SmeupInterpreterTest.kt
@@ -672,22 +672,4 @@ open class SmeupInterpreterTest : AbstractTest() {
         val expected = listOf("Lunghezza: 32580 Contenuto:                     -          -          -          -          -          -          -")
         assertEquals(expected, "smeup/T40_A30_P01".outputOf(configuration = smeupConfig))
     }
-
-    @Test
-    fun executeT10_A45_P04() {
-        val expected = listOf("IND(0)")
-        assertEquals(expected, "smeup/T10_A45_P04".outputOf())
-    }
-
-    @Test
-    fun executeT10_A45_P05() {
-        val expected = listOf("IND(1)")
-        assertEquals(expected, "smeup/T10_A45_P05".outputOf())
-    }
-
-    @Test
-    fun executeT10_A45_P06() {
-        val expected = listOf("IND(0)")
-        assertEquals(expected, "smeup/T10_A45_P06".outputOf())
-    }
 }
--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT10BaseCodopTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT10BaseCodopTest.kt
@@ -1,3 +1,36 @@
 package com.smeup.rpgparser.smeup
 
-open class MULANGT10BaseCodopTest : MULANGTTest()
+import org.junit.Test
+import kotlin.test.assertEquals
+
+open class MULANGT10BaseCodopTest : MULANGTTest() {
+    /**
+     * Check con indicatore con verifica a buon fine
+     * @see #246
+     */
+    @Test
+    fun executeT10_A45_P04() {
+        val expected = listOf("IND(0)")
+        assertEquals(expected, "smeup/T10_A45_P04".outputOf())
+    }
+
+    /**
+     * Check con indicatore con verifica non ok
+     * @see #246
+     */
+    @Test
+    fun executeT10_A45_P05() {
+        val expected = listOf("IND(1)")
+        assertEquals(expected, "smeup/T10_A45_P05".outputOf())
+    }
+
+    /**
+     * Check con indicatore di errore
+     * @see #246
+     */
+    @Test
+    fun executeT10_A45_P06() {
+        val expected = listOf("IND(0)")
+        assertEquals(expected, "smeup/T10_A45_P06".outputOf())
+    }
+}
