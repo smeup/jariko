898f31d3299add259b91d16c077dd3482f0b7422
Davide Palladino <davide.palladino@apuliasoft.com>
2024-03-19 08:06:30 +0100

    Moved test into opportune file

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/evaluation/SmeupInterpreterTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/evaluation/SmeupInterpreterTest.kt
@@ -666,10 +666,4 @@ open class SmeupInterpreterTest : AbstractTest() {
         val expected = listOf("Lunghezza: 32580 Contenuto:                     -          -          -          -          -          -          -")
         assertEquals(expected, "smeup/T40_A30_P01".outputOf(configuration = smeupConfig))
     }
-
-    @Test
-    fun executeT02_A80_P05() {
-        val expected = listOf("ABCDEFGHIJ123.00000")
-        assertEquals(expected, "smeup/T02_A80_P05".outputOf(configuration = smeupConfig))
-    }
 }
--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT02ConstAndDSpecTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT02ConstAndDSpecTest.kt
@@ -1,3 +1,16 @@
 package com.smeup.rpgparser.smeup
 
-open class MULANGT02ConstAndDSpecTest : MULANGTTest()
+import org.junit.Test
+import kotlin.test.assertEquals
+
+open class MULANGT02ConstAndDSpecTest : MULANGTTest() {
+    /**
+     * Data reference - Definizione in line e con prefix External DS
+     * @see #254
+     */
+    @Test
+    fun executeT02_A80_P05() {
+        val expected = listOf("ABCDEFGHIJ123.00000")
+        assertEquals(expected, "smeup/T02_A80_P05".outputOf(configuration = smeupConfig))
+    }
+}
