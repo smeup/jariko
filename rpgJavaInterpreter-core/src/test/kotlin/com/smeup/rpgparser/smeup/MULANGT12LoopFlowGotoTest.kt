1fe4f109495b0ec86649fe01d0ce414aab9f8036
Davide Palladino <davide.palladino@apuliasoft.com>
2024-03-26 15:02:00 +0100

    Fixed test

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT12LoopFlowGotoTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT12LoopFlowGotoTest.kt
@@ -1,6 +1,5 @@
 package com.smeup.rpgparser.smeup
 
-import com.smeup.rpgparser.assertCanBeParsed
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -11,7 +10,6 @@ open class MULANGT12LoopFlowGotoTest : MULANGTTest() {
      */
     @Test
     fun executeT12_A04_P18() {
-//        assertCanBeParsed("smeup/T12_A04_P18", printTree = true)
         val expected = listOf("A04_N50_CNT(10) A04_STR(1) A04_END(10) A04_CNT(11)")
         assertEquals(expected, "smeup/T12_A04_P18".outputOf())
     }
