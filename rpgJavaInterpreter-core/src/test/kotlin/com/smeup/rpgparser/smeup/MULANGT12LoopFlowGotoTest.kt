9311b9de96b040b6f5d48366a08f1ed9e550a74f
Davide Palladino <davide.palladino@apuliasoft.com>
2024-03-26 15:19:44 +0100

    Created `T12_A04_P19` test for task #264

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT12LoopFlowGotoTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT12LoopFlowGotoTest.kt
@@ -13,4 +13,14 @@ open class MULANGT12LoopFlowGotoTest : MULANGTTest() {
         val expected = listOf("A04_N50_CNT(10) A04_STR(1) A04_END(10) A04_CNT(11)")
         assertEquals(expected, "smeup/T12_A04_P18".outputOf())
     }
+
+    /**
+     * If followed by Do
+     * @see #264
+     */
+    @Test
+    fun executeT12_A04_P19() {
+        val expected = listOf("DO_1(A04_N50_CNT(10) A04_STR(1) A04_END(10) A04_CNT(11)) DO_2(A04_N50_CNT(15) A04_CNT(16))")
+        assertEquals(expected, "smeup/T12_A04_P19".outputOf())
+    }
 }
