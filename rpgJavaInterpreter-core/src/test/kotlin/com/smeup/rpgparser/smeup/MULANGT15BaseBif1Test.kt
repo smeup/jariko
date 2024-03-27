79617f89e7f30b95487b3404dea2f35dffe47166
Davide Palladino <davide.palladino@apuliasoft.com>
2024-03-27 15:49:57 +0100

    Fixed name of test

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT15BaseBif1Test.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT15BaseBif1Test.kt
@@ -9,7 +9,7 @@ open class MULANGT15BaseBif1Test : MULANGTTest() {
      * @see #261
      */
     @Test
-    fun executeT02_A50_P10() {
+    fun executeT15_A80_P09() {
         val expected = listOf("1(10 - North York) 2(5 - North) 3(15 - North          )")
         assertEquals(expected, "smeup/T15_A80_P09".outputOf())
     }
