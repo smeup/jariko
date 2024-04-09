090e76cd07203d80eaabbc3b1363c311438a7e18
Davide Palladino <davide.palladino@apuliasoft.com>
2024-04-09 11:02:58 +0200

    Implemented `MU711001` test with `£OAV`

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT70CompilationDirectiveTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT70CompilationDirectiveTest.kt
@@ -1,3 +1,22 @@
 package com.smeup.rpgparser.smeup
 
-open class MULANGT70CompilationDirectiveTest : MULANGTTest()
+import org.junit.Test
+import kotlin.test.assertEquals
+
+open class MULANGT70CompilationDirectiveTest : MULANGTTest() {
+    /**
+     * ###################
+     * ATOMIC TEST SECTION
+     * ###################
+     */
+
+    /**
+     * Data reference £DEC £OAV £HEX.
+     * @see #265
+     */
+    @Test
+    fun executeMU711001() {
+        val expected = listOf("Codice: ATTVTSD8*DMY250324G/10")
+        assertEquals(expected, "smeup/MU711001".outputOf(configuration = smeupConfig))
+    }
+}
