d5a89250e0a157e3811f9c7cd2de0422f866d783
Davide Palladino <davide.palladino@apuliasoft.com>
2024-04-09 14:16:40 +0200

    Removed `MU711001` test with `£OAV`

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT70CompilationDirectiveTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT70CompilationDirectiveTest.kt
@@ -1,22 +1,3 @@
 package com.smeup.rpgparser.smeup
 
-import org.junit.Test
-import kotlin.test.assertEquals
-
-open class MULANGT70CompilationDirectiveTest : MULANGTTest() {
-    /**
-     * ###################
-     * ATOMIC TEST SECTION
-     * ###################
-     */
-
-    /**
-     * Data reference £DEC £OAV £HEX.
-     * @see #265
-     */
-    @Test
-    fun executeMU711001() {
-        val expected = listOf("Codice: ATTVTSD8*DMY250324G/10")
-        assertEquals(expected, "smeup/MU711001".outputOf(configuration = smeupConfig))
-    }
-}
+open class MULANGT70CompilationDirectiveTest : MULANGTTest()
