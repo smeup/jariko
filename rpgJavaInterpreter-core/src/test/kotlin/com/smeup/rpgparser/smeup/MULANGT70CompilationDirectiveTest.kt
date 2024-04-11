387868dc0d0199f218dd08be1b3a9d8a04b72955
Davide Palladino <davide.palladino@apuliasoft.com>
2024-04-11 08:38:39 +0200

    Implemented `MU711001` test with its mock

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT70CompilationDirectiveTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT70CompilationDirectiveTest.kt
@@ -1,3 +1,16 @@
 package com.smeup.rpgparser.smeup
 
-open class MULANGT70CompilationDirectiveTest : MULANGTTest()
+import org.junit.Test
+import kotlin.test.assertEquals
+
+open class MULANGT70CompilationDirectiveTest : MULANGTTest() {
+    /**
+     * Data definition under dependencies.
+     * @see #265
+     */
+    @Test
+    fun executeMU711001() {
+        val expected = listOf("Codice: FOO")
+        assertEquals(expected, "smeup/MU711001".outputOf(configuration = smeupConfig))
+    }
+}
