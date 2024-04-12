1501d61bec6a477cf53fcd49d00c6ab2641f54e1
Davide Palladino <davide.palladino@apuliasoft.com>
2024-04-12 14:28:21 +0200

    Implemented test

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT04EssentialsCodopAndBifTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT04EssentialsCodopAndBifTest.kt
@@ -1,10 +1,27 @@
 package com.smeup.rpgparser.smeup
 
+import com.smeup.rpgparser.parsing.parsetreetoast.AstResolutionError
 import org.junit.Test
 import java.time.LocalDateTime
+import kotlin.test.Ignore
 import kotlin.test.assertEquals
+import kotlin.test.assertFails
+import kotlin.test.assertFailsWith
 
 open class MULANGT04EssentialsCodopAndBifTest : MULANGTTest() {
+    /**
+     * No error when there isn't a called subroutine
+     * WARNING: Ignored because there are conflicts between the dependencies.
+     * @see #270
+     */
+    @Test
+    @Ignore
+    fun executeMU041004() {
+        assertFailsWith(AstResolutionError::class) {
+            outputOf("smeup/MU041004")
+        }
+    }
+
     /**
      * TIME with inline number declarations
      */
