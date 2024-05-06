391450cc72447a7daff3c82190bbd589b50d4481
Davide Palladino <davide.palladino@apuliasoft.com>
2024-05-06 15:09:23 +0200

    Implemented test

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT50FileAccess1Test.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT50FileAccess1Test.kt
@@ -1,3 +1,16 @@
 package com.smeup.rpgparser.smeup
 
-open class MULANGT50FileAccess1Test : MULANGTTest()
+import org.junit.Test
+import kotlin.test.assertEquals
+
+open class MULANGT50FileAccess1Test : MULANGTTest() {
+    /**
+     * Data Reference to DS
+     * @see #280
+     */
+    @Test
+    fun executeMU500802() {
+        val expected = listOf("")
+        assertEquals(expected, "smeup/MU500802".outputOf(configuration = smeupConfig))
+    }
+}
