bb9f57a7cf17b4f2b28657aa48c7dabb1003358c
andreagosti9 <andrea.agosti9@gmail.com>
2024-06-03 14:33:49 +0200

    marked as todo

--- dspfparser/src/test/kotlin/com/smeup/dspfparser/linesclassifier/B1601av.kt
+++ dspfparser/src/test/kotlin/com/smeup/dspfparser/linesclassifier/B1601av.kt
@@ -1,10 +1,5 @@
 package com.smeup.dspfparser.linesclassifier
 
-import kotlin.test.Test
-
 internal class B1601av : DSPFSpecificationsLoader("./src/test/resources/b£1601av.dspf") {
-    @Test
-    fun tt() {
-        //
-    }
+    // TODO add test cases
 }
