9d1034cca2d2260786b47da283cad5cf7116cb79
andreagosti9 <andrea.agosti9@gmail.com>
2024-06-03 15:47:35 +0200

    made sure instance is correctly serialized/deserialized

--- dspfparser/src/test/kotlin/com/smeup/dspfparser/serialization/SerializationDeserializationTest.kt
+++ dspfparser/src/test/kotlin/com/smeup/dspfparser/serialization/SerializationDeserializationTest.kt
@@ -1,18 +1,21 @@
 package com.smeup.dspfparser.serialization
 
+import com.smeup.dspfparser.linesclassifier.DSPFSpecifications
 import com.smeup.dspfparser.linesclassifier.DSPFSpecificationsLoader
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import org.junit.jupiter.api.assertDoesNotThrow
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 internal class SerializationDeserializationTest : DSPFSpecificationsLoader("./src/test/resources/D2.dspf") {
+    private val json = Json { prettyPrint = true }
+
     @Test
     fun serialize() {
-        assertDoesNotThrow {
-            val prettyJson = Json { prettyPrint = true }
-            val json = prettyJson.encodeToString(this.specifications)
-            // TODO check if deserialized object is same as initial object
-        }
+        val initial = this.specifications
+        val string = json.encodeToString(initial)
+        val deserialized = json.decodeFromString<DSPFSpecifications>(string)
+        assertEquals(initial, deserialized)
     }
 }
