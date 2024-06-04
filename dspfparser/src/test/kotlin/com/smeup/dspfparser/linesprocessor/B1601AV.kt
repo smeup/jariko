3d648d8352b58dffa164abff5f672fe55c418f0e
andreagosti9 <andrea.agosti9@gmail.com>
2024-06-04 10:34:34 +0200

    fixed tests

--- dspfparser/src/test/kotlin/com/smeup/dspfparser/linesprocessor/B1601AV.kt
+++ dspfparser/src/test/kotlin/com/smeup/dspfparser/linesprocessor/B1601AV.kt
@@ -1,11 +1,10 @@
 package com.smeup.dspfparser.linesprocessor
 
-import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import kotlin.test.Test
 
 internal class B1601AV : LinesProcessorLoader("./src/test/resources/B£1601AV.dspf") {
     @Test
     fun doesNotThrowErrors() {
-        assertDoesNotThrow { this.linesProcessor.createLines() }
+        this.linesProcessor.createLines()
     }
 }
--- dspfparser/src/test/kotlin/com/smeup/dspfparser/linesprocessor/B1601BV.kt
+++ dspfparser/src/test/kotlin/com/smeup/dspfparser/linesprocessor/B1601BV.kt
@@ -1,11 +1,10 @@
 package com.smeup.dspfparser.linesprocessor
 
-import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import kotlin.test.Test
 
 internal class B1601BV : LinesProcessorLoader("./src/test/resources/B£1601BV.dspf") {
     @Test
     fun doesNotThrowErrors() {
-        assertDoesNotThrow { this.linesProcessor.createLines() }
+        this.linesProcessor.createLines()
     }
 }
