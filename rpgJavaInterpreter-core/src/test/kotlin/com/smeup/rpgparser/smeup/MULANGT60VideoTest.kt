8c6f987112b64d2bbc223f1ee3b015fca686db20
andreagosti9 <andrea.agosti9@gmail.com>
2024-06-07 11:28:20 +0200

    made MULANGT60VideoTest work

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT60VideoTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT60VideoTest.kt
@@ -6,13 +6,13 @@ import kotlin.test.assertEquals
 
 open class MULANGT60VideoTest : MULANGTTest() {
 
-    @Test @Ignore("Currently ignored to fix compileAllMutes")
+    @Test
     fun executeT60_A10_P01() {
         val expected = listOf<String>("KA(0)KB(0)KC(0)KD(0)KE(0)KF(0)KG(0)KH(0)KI(0)KJ(0)KK(0)KL(0)KM(0)KN(0)KP(0)KQ(0)KR(0)KS(0)KT(0)KU(0)KV(0)KW(0)KX(0)KY(0)")
         assertEquals(expected, "smeup/T60_A10_P01".outputOf(configuration = smeupConfig))
     }
 
-    @Test @Ignore("Currently ignored to fix compileAllMutes")
+    @Test
     fun executeT60_A10_P01_02() {
         val expected = listOf<String>()
         assertEquals(expected, "smeup/T60_A10_P01-02".outputOf(configuration = smeupConfig))
@@ -22,7 +22,7 @@ open class MULANGT60VideoTest : MULANGTTest() {
      * Data reference - Inline definition and in video file
      * @see #252
      */
-    @Test @Ignore("Currently ignored to fix compileAllMutes")
+    @Test
     fun executeT60_A40_P01() {
         val expected = listOf("5")
         assertEquals(expected, "smeup/T60_A40_P01".outputOf(configuration = smeupConfig))
