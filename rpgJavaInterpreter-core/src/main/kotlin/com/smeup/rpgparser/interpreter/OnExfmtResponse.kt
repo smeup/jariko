13f33341a1e68c3cd48222de8deb2299e888eae6
andreagosti9 <andrea.agosti9@gmail.com>
2024-06-10 16:02:52 +0200

    added OnExfmtResponse and RuntimeInterpreterSnapshot

---
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/OnExfmtResponse.kt
@@ -0,0 +1,9 @@
+package com.smeup.rpgparser.interpreter
+
+/**
+ * Represents the return state by an EXFMT statement
+ */
+data class OnExfmtResponse(
+    val runtimeInterpreterSnapshot: RuntimeInterpreterSnapshot,
+    val values: Map<String, String>
+){}
---
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/RuntimeInterpreterSnapshot.kt
@@ -0,0 +1,9 @@
+package com.smeup.rpgparser.interpreter
+
+/**
+ * An instance of this object will be used by the client to inform jariko about
+ * interpreter state just before the EXFMT statement execution.
+ */
+class RuntimeInterpreterSnapshot {
+
+}
