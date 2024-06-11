3d23a2f3cf5bce997b95e542141001460c9ce3bf
andreagosti9 <andrea.agosti9@gmail.com>
2024-06-11 15:12:50 +0200

    introduced base test

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/video/VideoInterpeterTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/video/VideoInterpeterTest.kt
@@ -26,15 +26,6 @@ class VideoInterpeterTest : AbstractTest() {
     }
 
     @Test
-    fun executeFILEDEF() {
-        val expected = listOf("W\$PERI:12", "£RASDI:HELLO_WORLD")
-        configuration.jarikoCallback.afterAstCreation = {
-            assertNotNull(it.displayFiles?.get("FILEDEFV"))
-        }
-        assertEquals(expected = expected, actual = "video/FILEDEF".outputOf(configuration = configuration))
-    }
-
-    @Test
     fun executeEXFMT_MOCK() {
         val expected = listOf("")
         configuration.jarikoCallback.afterAstCreation = {
@@ -59,5 +50,23 @@ class VideoInterpeterTest : AbstractTest() {
             assertNotNull(it.displayFiles?.get("UNLOCK_MV"))
         }
         assertEquals(expected = expected, actual = "video/UNLOCK_MOCK".outputOf(configuration = configuration))
+    }
+
+    @Test
+    fun executeFILEDEF() {
+        val expected = listOf("W\$PERI:12", "£RASDI:HELLO_WORLD")
+        configuration.jarikoCallback.afterAstCreation = {
+            assertNotNull(it.displayFiles?.get("FILEDEFV"))
+        }
+        assertEquals(expected = expected, actual = "video/FILEDEF".outputOf(configuration = configuration))
+    }
+
+    @Test
+    fun executeEXFMT1() {
+        val expected = listOf("")
+        configuration.jarikoCallback.afterAstCreation = {
+            assertNotNull(it.displayFiles?.get("EXFMT1V"))
+        }
+        assertEquals(expected = expected, actual = "video/EXFMT1".outputOf(configuration = configuration))
     }
 }
---
+++ rpgJavaInterpreter-core/src/test/resources/video/EXFMT1.rpgle
@@ -0,0 +1,5 @@
+     FEXFMT1V   CF   E             WORKSTN USROPN
+     D MSG             S             50          VARYING
+     C                   EXFMT      FMT1
+     C                   EVAL       MSG=''
+     C     MSG           DSPLY
