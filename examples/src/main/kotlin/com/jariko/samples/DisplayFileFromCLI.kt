ac1af429c44c171293f9c6ead68c5815c5f7404e
andreagosti9 <andrea.agosti9@gmail.com>
2024-06-18 10:05:46 +0200

    made basic working example

---
+++ examples/src/main/kotlin/com/jariko/samples/DisplayFileFromCLI.kt
@@ -0,0 +1,105 @@
+package com.jariko.samples
+
+import com.smeup.dspfparser.linesclassifier.DSPFField
+import com.smeup.dspfparser.linesclassifier.DSPFFieldType
+import com.smeup.rpgparser.execution.Configuration
+import com.smeup.rpgparser.execution.DspfConfig
+import com.smeup.rpgparser.execution.JarikoCallback
+import com.smeup.rpgparser.execution.Options
+import com.smeup.rpgparser.execution.SimpleDspfConfig
+import com.smeup.rpgparser.execution.getProgram
+import com.smeup.rpgparser.interpreter.DecimalValue
+import com.smeup.rpgparser.interpreter.IntValue
+import com.smeup.rpgparser.interpreter.OnExfmtResponse
+import com.smeup.rpgparser.interpreter.RuntimeInterpreterSnapshot
+import com.smeup.rpgparser.interpreter.StringValue
+import com.smeup.rpgparser.interpreter.Value
+import com.smeup.rpgparser.rpginterop.DirRpgProgramFinder
+import java.io.File
+
+// UI control functions
+
+private fun parseInput(input: String): Map<String, String> {
+    val assignments = input.split(';')
+    val variablesAndValues = mutableMapOf<String, String>()
+
+    assignments.forEach {
+        val tokens = it.split('=')
+        variablesAndValues[tokens[0]] = tokens[1]
+    }
+
+    return variablesAndValues
+}
+
+private fun printOutputFields(fields: List<DSPFField>) {
+    print("OUTPUT fields:\t")
+    fields.filter { it.type == DSPFFieldType.OUTPUT }.forEach {
+        print("${it.name} = ${(it.value as Value).asString().value}")
+    }
+    print("\t")
+}
+
+private fun askInputFor(fields: List<DSPFField>): Map<String, Value> {
+    val variablesAndValues = mutableMapOf<String, Value>()
+    val line = readln()
+    val updatedVariables = parseInput(line)
+
+    fields.filter { it.type == DSPFFieldType.INPUT && updatedVariables[it.name] != null }.forEach {
+        if (it.isNumeric && it.precision!! == 0)
+            variablesAndValues[it.name] = IntValue(updatedVariables[it.name]!!.toLong())
+        if (it.isNumeric && it.precision!! > 0)
+            variablesAndValues[it.name] = DecimalValue(updatedVariables[it.name]!!.toBigDecimal())
+        else if (!it.isNumeric)
+            variablesAndValues[it.name] = StringValue(updatedVariables[it.name]!!)
+    }
+
+    return variablesAndValues
+}
+
+private fun onExfmt(fields: List<DSPFField>, runtimeInterpreterSnapshot: RuntimeInterpreterSnapshot): OnExfmtResponse? {
+    printOutputFields(fields)
+    while (true) {
+        try {
+            val variablesAndValues = askInputFor(fields)
+            return OnExfmtResponse(runtimeInterpreterSnapshot, variablesAndValues)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            continue
+        }
+    }
+}
+
+// Jariko call setup functions
+
+private fun createOptions(): Options {
+    return Options(callProgramHandler = createCallProgramHandler())
+}
+
+private fun createDspfConfig(): DspfConfig {
+    val simpleDspfConfig = SimpleDspfConfig({ }.javaClass.getResource("/metadata")!!.path)
+    return DspfConfig(
+        metadataProducer = simpleDspfConfig::getMetadata,
+        dspfProducer = simpleDspfConfig::dspfProducer
+    )
+}
+
+private fun createJarikoCallback(): JarikoCallback {
+    val jarikoCallback = JarikoCallback()
+    jarikoCallback.onExfmt = ::onExfmt
+    return jarikoCallback
+}
+
+private fun createConfig(): Configuration {
+    return Configuration(
+        options = createOptions(),
+        dspfConfig = createDspfConfig(),
+        jarikoCallback = createJarikoCallback()
+    )
+}
+
+fun main() {
+    val programFinders = listOf(DirRpgProgramFinder(File({ }.javaClass.getResource("/rpg")!!.path)),)
+    val program = getProgram(nameOrSource = "ADD01.rpgle", programFinders = programFinders)
+
+    program.singleCall(emptyList(), configuration = createConfig())
+}
