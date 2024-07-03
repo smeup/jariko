968c0ff17ce9d71f4f405fd1eed5f6f0f02ed1bd
Davide Palladino <davide.palladino@apuliasoft.com>
2024-07-03 11:42:01 +0200

    Implemented test and its dependency

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT18ProcedureTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT18ProcedureTest.kt
@@ -1,3 +1,16 @@
 package com.smeup.rpgparser.smeup
 
-open class MULANGT18ProcedureTest : MULANGTTest()
+import org.junit.Test
+import kotlin.test.assertEquals
+
+open class MULANGT18ProcedureTest : MULANGTTest() {
+    /**
+     * LIKE to variable defined into a COPY. This one is also declared inner of procedure.
+     * @see #LS24003187
+     */
+    @Test
+    fun executeT10_A20_P52() {
+        val expected = listOf("O:   HT     -P:HT_P")
+        assertEquals(expected, "smeup/MU181003".outputOf(configuration = smeupConfig))
+    }
+}
