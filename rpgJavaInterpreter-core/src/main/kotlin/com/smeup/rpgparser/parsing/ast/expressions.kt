f7d049506264001c835a881630354b011dabe1df
domenico <domenico.mancini@apuliasoft.com>
2024-08-07 10:01:24 +0200

    Make ArrayAccessExpr fields mutable

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/parsing/ast/expressions.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/parsing/ast/expressions.kt
@@ -333,7 +333,7 @@ data class QualifiedAccessExpr(val container: Expression, val field: ReferenceBy
 }
 
 @Serializable
-data class ArrayAccessExpr(val array: Expression, val index: Expression, override val position: Position? = null) :
+data class ArrayAccessExpr(var array: Expression, var index: Expression, override val position: Position? = null) :
     AssignableExpression(position) {
     override fun render(): String {
         return "${this.array.render()}(${index.render()}))"
