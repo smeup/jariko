7e0892c32f407f0f5051312fe0ea40a09e8ba5f8
domenico <domenico.mancini@apuliasoft.com>
2024-08-19 14:31:05 +0200

    Add runtime error test case

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/interpreter/JarikoCallbackTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/interpreter/JarikoCallbackTest.kt
@@ -26,6 +26,7 @@ import com.smeup.rpgparser.parsing.facade.SourceReference
 import com.smeup.rpgparser.parsing.facade.SourceReferenceType
 import com.smeup.rpgparser.parsing.parsetreetoast.ToAstConfiguration
 import com.smeup.rpgparser.rpginterop.DirRpgProgramFinder
+import com.smeup.rpgparser.smeup.dbmock.TABDS01LDbMock
 import com.smeup.rpgparser.utils.Format
 import com.smeup.rpgparser.utils.compile
 import org.junit.Assert
@@ -660,6 +661,32 @@ class JarikoCallbackTest : AbstractTest() {
         executeSourceLineTest(pgm = "ERROR35")
     }
 
+    /**
+     * NOTE: This is error is thrown because Reload does not support '*START' and '*END' constants yet.
+     * When this feature gets supported please restore it on Jariko side by following these steps:
+     * - Remove this test or mark it as ignored
+     * - Remove the [@Ignore] decorator from the [MULANGT50FileAccess1Test.executeMUDRNRAPU00248] test
+     * - Remove the runtime error (it should be in [Expression.createKList] if not moved)
+     */
+    @Test
+    fun executeERROR36CallBackTest() {
+        TABDS01LDbMock().use {
+            com.smeup.rpgparser.db.utilities.execute(listOf(it.createTable(), it.populateTable()))
+            executePgmCallBackTest(
+                pgm = "ERROR36",
+                sourceReferenceType = SourceReferenceType.Program,
+                sourceId = "ERROR36",
+                lines = listOf(6),
+                reloadConfig = createTABDS01LReloadConfig()
+            )
+        }
+    }
+
+    @Test
+    fun executeERROR36SourceLineTest() {
+        executeSourceLineTest(pgm = "ERROR36")
+    }
+
     @Test
     fun bypassSyntaxErrorTest() {
         val configuration = Configuration().apply {
@@ -832,5 +859,30 @@ class JarikoCallbackTest : AbstractTest() {
         )
         val metadataProducer = { file: String -> metadata[file]!! }
         return ReloadConfig(DBNativeAccessConfig(connectionsConfig = emptyList()), metadataProducer = metadataProducer)
+    }
+
+    private fun createTABDS01LReloadConfig(): ReloadConfig {
+        val metadata = mapOf(
+            "TABDS01L" to FileMetadata(
+                name = "TABDS01L",
+                tableName = "TABDS00F",
+                recordFormat = "TABDSR",
+                fields = listOf(
+                    DbField("S\$SETT", StringType(3)),
+                    DbField("S\$SUBS", StringType(2))
+                ),
+                accessFields = listOf("S\$SETT", "S\$SUBS")
+            )
+        )
+        val metadataProducer = { file: String -> metadata[file]!! }
+        return ReloadConfig(DBNativeAccessConfig(connectionsConfig = listOf(
+            com.smeup.dbnative.ConnectionConfig(
+                fileName = "*",
+                url = "jdbc:hsqldb:hsql://127.0.0.1:9001/mainDb",
+                user = "SA",
+                password = "",
+                driver = "org.hsqldb.jdbc.JDBCDriver"
+            )
+        )), metadataProducer = metadataProducer)
     }
 }
