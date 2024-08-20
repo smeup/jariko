af1ea390cd08318665d469f471f2fa6b68be8968
domenico <domenico.mancini@apuliasoft.com>
2024-08-20 08:54:16 +0200

    Update superclasses

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/dbmock/MULANGTLDbMock.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/dbmock/MULANGTLDbMock.kt
@@ -3,7 +3,7 @@ package com.smeup.rpgparser.smeup.dbmock
 import com.smeup.rpgparser.interpreter.FileMetadata
 import java.io.File
 
-class MULANGTLDbMock : MetadataDbMock() {
+class MULANGTLDbMock : DbMock {
     override val metadata =
         FileMetadata.createInstance(File("src/test/resources/smeup/metadata/MULANGTL.json").inputStream())
 }
--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/dbmock/TABDS01LDbMock.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/dbmock/TABDS01LDbMock.kt
@@ -3,7 +3,7 @@ package com.smeup.rpgparser.smeup.dbmock
 import com.smeup.rpgparser.interpreter.FileMetadata
 import java.io.File
 
-class TABDS01LDbMock : MetadataDbMock() {
+class TABDS01LDbMock : DbMock {
     override val metadata =
         FileMetadata.createInstance(File("src/test/resources/smeup/metadata/TABDS01L.json").inputStream())
 }
