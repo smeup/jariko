e41dc888052d5c688695709f81809073433f6056
Davide Palladino <davide.palladino@apuliasoft.com>
2024-09-30 14:04:02 +0200

    Applied fix for `DOU`

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/parsing/parsetreetoast/statements.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/parsing/parsetreetoast/statements.kt
@@ -62,7 +62,7 @@ internal fun BlockContext.toAst(conf: ToAstConfiguration = ToAstConfiguration())
             .buildIndicatorsFlags(this.csDOWxx(), conf)
         this.forstatement() != null -> this.forstatement().toAst(conf)
             .buildIndicatorsFlags(this.forstatement().beginfor(), conf)
-        this.begindou() != null -> this.begindou().toAst(blockContext = this, conf = conf)
+        this.begindou() != null -> this.begindou().toAst(blockContext = this, conf = conf).buildIndicatorsFlags(this.begindou(), conf)
         this.csDOUxx() != null -> this.csDOUxx().toAst(blockContext = this, conf = conf)
             .buildIndicatorsFlags(this.csDOUxx(), conf)
         this.monitorstatement() != null -> this.monitorstatement().let {
@@ -109,6 +109,8 @@ private fun <TContext : ParserRuleContext> Statement.buildIndicatorsFlags(contex
         is CsDOUxxContext -> context.toIndicatorCondition(context.indicators, context.indicatorsOff, conf)
             .also { populateContinuedIndicators(context.cspec_continuedIndicators()) }
         is BegindoContext -> context.toIndicatorCondition(context.indicators, context.indicatorsOff, conf)
+            .also { populateContinuedIndicators(context.cspec_continuedIndicators()) }
+        is BegindouContext -> context.toIndicatorCondition(context.indicators, context.indicatorsOff, conf)
             .also { populateContinuedIndicators(context.cspec_continuedIndicators()) }
         is BeginforContext -> context.toIndicatorCondition(context.indicators, context.indicatorsOff, conf)
             .also { populateContinuedIndicators(context.cspec_continuedIndicators()) }
