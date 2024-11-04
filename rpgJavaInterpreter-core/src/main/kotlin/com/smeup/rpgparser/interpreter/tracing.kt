3034ee6c1b62d110dceeb3b4c2f2f1bfee544e04
domenico <domenico.mancini@apuliasoft.com>
2024-11-04 14:03:39 +0100

    Add new callbacks

---
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/tracing.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 Sme.UP S.p.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * imitations under the License.
+ *
+ */
+
+package com.smeup.rpgparser.interpreter
+
+/**
+ * Kind of a trace
+ */
+enum class JarikoTraceKind {
+    Parsing,
+    SymbolTable,
+    CompositeStatement,
+    Program
+}
+
+/**
+ * A trace emitted by Jariko for telemetry purposes
+ */
+data class JarikoTrace(
+    val kind: JarikoTraceKind,
+    val description: String? = null
+)
+
+/**
+ * A trace emitted by an RPG program for telemetry purposes
+ */
+data class RpgTrace(
+    val program: String,
+    val description: String? = null
+)
