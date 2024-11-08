a5d1d5f9b9b63f30665913f6b260fa610b7cd98e
Davide Palladino <davide.palladino@apuliasoft.com>
2024-11-08 13:38:15 +0100

    Added comment

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/mock/ExecSqlStmtMockTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/mock/ExecSqlStmtMockTest.kt
@@ -5,6 +5,12 @@ import org.junit.Test
 import kotlin.test.assertEquals
 
 class ExecSqlStmtMockTest : MockTest() {
+    /**
+     * This program simulates a simple SQL FETCH operation, using SQLCOD to
+     * display the SQL status before and after execution. SQLCOD is explicitly
+     * defined here for Jariko, emulating AS400 behavior. Suitable for testing
+     * basic SQL handling in a mock setup.
+     */
     @Test
     fun executeEXECSQL_SQLCOD100() {
         val expected = listOf("0", "100")
