726deaaccd7ae443d6427048a29c2acc690e93a0
Davide Palladino <davide.palladino@apuliasoft.com>
2024-11-29 18:02:17 +0100

    Added new test for testing the utilization of params between caller and called, in accord to official documentation of IBM

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT10BaseCodopTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT10BaseCodopTest.kt
@@ -674,4 +674,17 @@ open class MULANGT10BaseCodopTest : MULANGTTest() {
         val expected = listOf("Sub program", "1")
         assertEquals(expected, "smeup/MUDRNRAPU00172".outputOf(configuration = smeupConfig))
     }
+
+    /**
+     * This program tests the behaviour of `CALL` and `PLIST` when is used the Params for both between caller and called.
+     * In accord to documentation:
+     * - when `CALL` is processed, the content of Factor 2 is placed in the Result field;
+     * - when control transfers to called program, the contents of the Result field is placed in the Factor 1 field.
+     * @see #LS24005158
+     */
+    @Test
+    fun executeMUDRNRAPU00173() {
+        val expected = listOf("BAR")
+        assertEquals(expected, "smeup/MUDRNRAPU00173".outputOf(configuration = smeupConfig))
+    }
 }
