edfbddf70b7b79a282aa893556015a543e46c821
Davide Palladino <davide.palladino@apuliasoft.com>
2024-12-06 17:40:34 +0100

    Typo

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/ExpressionEvaluation.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/ExpressionEvaluation.kt
@@ -109,7 +109,6 @@ class ExpressionEvaluation(
     }
 
     override fun eval(expression: EqualityExpr): Value = proxyLogging(expression) {
-        val left = when (expression.right) {
         val left = when (expression.left) {
             /* In this case there is a comparison from *HIVAL and a generic Value. Is important to take the right "HIVAL" based of right side. */
             is HiValExpr -> expression.right.type().hiValue()
