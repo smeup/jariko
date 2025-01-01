1f222b6f59fea6a01a7beede6c7ba340d30afb68
lanarimarco <marco.lanari@smeup.com>
2025-01-01 22:21:23 +0100

    refactor

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/interpreter/datastruct_value_builder.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/interpreter/datastruct_value_builder.kt
@@ -4,97 +4,6 @@ import kotlin.random.Random
 import kotlin.test.*
 import kotlin.time.measureTime
 
-class IndexedStringBuilderTest {
-
-    @Test
-    fun replaceWithinSingleChunkFirst() {
-        val builder = IndexedStringBuilder("HelloWorld", 5)
-        builder.replace(0, 5, "12345")
-        assertEquals("12345World", builder.toString())
-    }
-
-    @Test
-    fun replaceWithinSingleChunkMiddle() {
-        val builder = IndexedStringBuilder("HelloWorldHello", 5)
-        builder.replace(5, 10, "12345")
-        assertEquals("Hello12345Hello", builder.toString())
-    }
-
-    @Test
-    fun replaceWithinSingleChunkLast() {
-        val builder = IndexedStringBuilder("HelloWorldHello", 5)
-        builder.replace(10, 15, "12345")
-        assertEquals("HelloWorld12345", builder.toString())
-    }
-
-    @Test
-    fun replaceWithinSubChunkFirst() {
-        val builder = IndexedStringBuilder("HelloWorld", 5)
-        builder.replace(0, 1, "1")
-        assertEquals("1elloWorld", builder.toString())
-        builder.replace(1, 2, "1")
-        assertEquals("11lloWorld", builder.toString())
-    }
-
-    @Test
-    fun replaceAllChunks() {
-        val builder = IndexedStringBuilder("HelloWorld", 5)
-        builder.replace(0, 10, "1234567890")
-        assertEquals("1234567890", builder.toString())
-    }
-
-    @Test
-    fun replaceAcrossMultipleChunks() {
-        val builder = IndexedStringBuilder("HelloWorld", 2)
-        builder.replace(3, 8, "12345")
-        assertEquals("Hel12345ld", builder.toString())
-    }
-
-    @Test
-    fun replaceWithReplacingStringGreaterThanReplacedRangeThrowsException() {
-        val builder = IndexedStringBuilder("HelloWorld", 5)
-        assertFailsWith<IllegalArgumentException> {
-            builder.replace(0, 5, "HelloWorld")
-        }
-    }
-
-    @Test
-    fun replaceWithReplacingStringLowerThanReplacedRangeThrowsException() {
-        val builder = IndexedStringBuilder("HelloWorld", 5)
-        assertFailsWith<IllegalArgumentException> {
-            builder.replace(0, 5, "H")
-        }
-    }
-
-    @Test
-    fun substringWithinSingleChunk() {
-        val builder = IndexedStringBuilder("HelloWorld", 5)
-        val result = builder.substring(0, 5)
-        assertEquals("Hello", result)
-    }
-
-    @Test
-    fun substringAcrossMultipleChunks() {
-        val builder = IndexedStringBuilder("HelloWorld", 5)
-        val result = builder.substring(3, 8)
-        assertEquals("loWor", result)
-    }
-
-    @Test
-    fun substringWithExactLength() {
-        val builder = IndexedStringBuilder("HelloWorld", 5)
-        val result = builder.substring(0, 10)
-        assertEquals("HelloWorld", result)
-    }
-
-    @Test
-    fun substringWithEmptyResult() {
-        val builder = IndexedStringBuilder("HelloWorld", 5)
-        val result = builder.substring(5, 5)
-        assertEquals("", result)
-    }
-}
-
 class DataStructValueBuilderTest {
 
     private fun replaceAllFields(
@@ -228,5 +137,96 @@ class DataStructValueBuilderTest {
         assertEquals(checkBuiltString, indexedStringBuilder.toString())
         val ratio = stringBuilderWrapperDuration.div(indexedStringBuilderDuration)
         assertTrue { ratio > 10 }
+    }
+}
+
+class IndexedStringBuilderTest {
+
+    @Test
+    fun replaceWithinSingleChunkFirst() {
+        val builder = IndexedStringBuilder("HelloWorld", 5)
+        builder.replace(0, 5, "12345")
+        assertEquals("12345World", builder.toString())
+    }
+
+    @Test
+    fun replaceWithinSingleChunkMiddle() {
+        val builder = IndexedStringBuilder("HelloWorldHello", 5)
+        builder.replace(5, 10, "12345")
+        assertEquals("Hello12345Hello", builder.toString())
+    }
+
+    @Test
+    fun replaceWithinSingleChunkLast() {
+        val builder = IndexedStringBuilder("HelloWorldHello", 5)
+        builder.replace(10, 15, "12345")
+        assertEquals("HelloWorld12345", builder.toString())
+    }
+
+    @Test
+    fun replaceWithinSubChunkFirst() {
+        val builder = IndexedStringBuilder("HelloWorld", 5)
+        builder.replace(0, 1, "1")
+        assertEquals("1elloWorld", builder.toString())
+        builder.replace(1, 2, "1")
+        assertEquals("11lloWorld", builder.toString())
+    }
+
+    @Test
+    fun replaceAllChunks() {
+        val builder = IndexedStringBuilder("HelloWorld", 5)
+        builder.replace(0, 10, "1234567890")
+        assertEquals("1234567890", builder.toString())
+    }
+
+    @Test
+    fun replaceAcrossMultipleChunks() {
+        val builder = IndexedStringBuilder("HelloWorld", 2)
+        builder.replace(3, 8, "12345")
+        assertEquals("Hel12345ld", builder.toString())
+    }
+
+    @Test
+    fun replaceWithReplacingStringGreaterThanReplacedRangeThrowsException() {
+        val builder = IndexedStringBuilder("HelloWorld", 5)
+        assertFailsWith<IllegalArgumentException> {
+            builder.replace(0, 5, "HelloWorld")
+        }
+    }
+
+    @Test
+    fun replaceWithReplacingStringLowerThanReplacedRangeThrowsException() {
+        val builder = IndexedStringBuilder("HelloWorld", 5)
+        assertFailsWith<IllegalArgumentException> {
+            builder.replace(0, 5, "H")
+        }
+    }
+
+    @Test
+    fun substringWithinSingleChunk() {
+        val builder = IndexedStringBuilder("HelloWorld", 5)
+        val result = builder.substring(0, 5)
+        assertEquals("Hello", result)
+    }
+
+    @Test
+    fun substringAcrossMultipleChunks() {
+        val builder = IndexedStringBuilder("HelloWorld", 5)
+        val result = builder.substring(3, 8)
+        assertEquals("loWor", result)
+    }
+
+    @Test
+    fun substringWithExactLength() {
+        val builder = IndexedStringBuilder("HelloWorld", 5)
+        val result = builder.substring(0, 10)
+        assertEquals("HelloWorld", result)
+    }
+
+    @Test
+    fun substringWithEmptyResult() {
+        val builder = IndexedStringBuilder("HelloWorld", 5)
+        val result = builder.substring(5, 5)
+        assertEquals("", result)
     }
 }
