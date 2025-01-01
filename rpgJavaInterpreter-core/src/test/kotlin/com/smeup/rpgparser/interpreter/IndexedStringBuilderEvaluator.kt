2ca4e8ebdd1b4891d202340b1989d2ec3b49a966
lanarimarco <marco.lanari@smeup.com>
2025-01-01 19:14:50 +0100

    chore: createPerformanceComparisonDataset compare the ratio both sb/indexed_sb and sb/ds_string_value

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/interpreter/IndexedStringBuilderEvaluator.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/interpreter/IndexedStringBuilderEvaluator.kt
@@ -1,7 +1,6 @@
 package com.smeup.rpgparser.interpreter
 
 import java.math.RoundingMode
-import kotlin.time.Duration
 import kotlin.time.DurationUnit
 import kotlin.time.measureTime
 
@@ -10,25 +9,19 @@ private fun stringBuilderVsDataStructStringValue(
     debugInformation: Boolean = false,
     stringSize: Int,
     fields: Int,
-    chunksSize: Int,
-    iterations: Int,
-    dataStructStringValue: (value: String, chunksSize: Int) -> DataStructStringValue = { value, chunksSizeParam ->
-        IndexedStringBuilder(
-            value,
-            chunksSizeParam
-        )
-    },
-    stringBuilderDone: (duration: Duration) -> Unit,
-    indexedStringBuilderDone: (duration: Duration) -> Unit
+    iterations: Int
 ) {
     require(stringSize >= fields) { "stringSize must be greater than or equal to elements" }
     require(stringSize % fields == 0) { "stringSize: $stringSize must a multiple of fields: $fields" }
 
+    val chunksSize = stringSize / fields
+
     if (printHeader) {
-        println("stringSize, ${if (debugInformation) "fields, " else "" }chunksSize, ratio${if (debugInformation) ", duration(ms)" else "" }")
+        println("stringSize, fields${if (debugInformation) ", chunksSize" else ""}, ratio(sb/indexed_sb), " +
+                "ratio(sb/ds_string_value)${if (debugInformation) ", duration(ms)" else ""}")
     }
 
-    print("$stringSize, ${if (debugInformation) "$fields, " else ""}$chunksSize")
+    print("$stringSize, $fields${if (debugInformation) ", $chunksSize" else ""}")
 
     val replacingString = "b".repeat(stringSize / fields)
     val sbDuration = measureTime {
@@ -42,10 +35,9 @@ private fun stringBuilderVsDataStructStringValue(
             }
         }
     }
-    stringBuilderDone(sbDuration)
 
     val indexedSbDuration = measureTime {
-        val indexedSb = dataStructStringValue("a".repeat(stringSize), chunksSize)
+        val indexedSb = IndexedStringBuilder("a".repeat(stringSize), chunksSize)
         for (i in 0 until iterations) {
             val replacingChars = stringSize / fields
             for (j in 0 until fields) {
@@ -56,29 +48,44 @@ private fun stringBuilderVsDataStructStringValue(
         }
     }
 
-    indexedStringBuilderDone(indexedSbDuration)
+    val dataStructStringValueDuration = measureTime {
+        val dataStructStringValue = DataStructStringValue.create("a".repeat(stringSize), fields)
+        for (i in 0 until iterations) {
+            val replacingChars = stringSize / fields
+            for (j in 0 until fields) {
+                val start: Int = j * replacingChars
+                val end: Int = start + replacingChars
+                dataStructStringValue.replace(start, end, replacingString)
+            }
+        }
+    }
 
-    val ratio = sbDuration.div(indexedSbDuration).toBigDecimal().setScale(2, RoundingMode.HALF_DOWN).toDouble()
-
-    println(", $ratio${if (debugInformation) ", ${sbDuration.plus(indexedSbDuration).toLong(DurationUnit.MILLISECONDS)}" else ""}")
+    val ratioSbIndexed = sbDuration.div(indexedSbDuration).toBigDecimal().setScale(2, RoundingMode.HALF_DOWN).toDouble()
+    val ratioSbDataStructStringValue = sbDuration.div(dataStructStringValueDuration).toBigDecimal().setScale(2, RoundingMode.HALF_DOWN).toDouble()
+    println(", $ratioSbIndexed, $ratioSbDataStructStringValue${if (debugInformation) ", ${sbDuration.plus(indexedSbDuration).toLong(DurationUnit.MILLISECONDS)}" else ""}")
 }
 
-private fun createPerformanceComparisonDataset(
-    chunksSize: (stringSize: Int, fields: Int) -> Int = { stringSize, fields -> stringSize / fields }
-) {
-    for (stringSize in listOf(10, 100, 500, 1000, 2_000, 5_000, 10_000, 50_000, 100_000, 1_000_000, 10_000_000)) {
+private fun createPerformanceComparisonDataset() {
+    val stringSizeTests = mutableListOf<Int>()
+    val startStringSizeValue = 1000
+    var value = startStringSizeValue
+    while (value <= 100_000) {
+        for (i in 1..9) {
+            stringSizeTests.add(value * i)
+        }
+        value *= 10
+    }
+
+    for (stringSize in stringSizeTests) {
         for (fields in listOf(1, 2, 5, 10, 20, 50, 100, 200, 500, 1000)) {
             if (stringSize < fields || ((stringSize % fields) != 0)) {
                 break
             }
             stringBuilderVsDataStructStringValue(
-                printHeader = stringSize == 10 && fields == 1,
+                printHeader = stringSize == startStringSizeValue && fields == 1,
                 stringSize = stringSize,
                 fields = fields,
-                chunksSize = chunksSize(stringSize, fields),
-                iterations = if (stringSize * fields < 100_000) 1_000_000 else if (stringSize * fields < 1_000_000) 100_000 else 100,
-                stringBuilderDone = { },
-                indexedStringBuilderDone = { }
+                iterations = if (stringSize * fields < 100_000) 1_000_000 else if (stringSize * fields < 1_000_000) 100_000 else 100
             )
         }
     }
