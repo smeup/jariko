422c4b0421e8044f842f19a803da50d8ac570bf6
lanarimarco <marco.lanari@smeup.com>
2025-01-01 13:08:07 +0100

    refactor: move IndexedStringBuilder comparison logic

---
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/interpreter/IndexedStringBuilderEvaluator.kt
@@ -0,0 +1,79 @@
+package com.smeup.rpgparser.interpreter
+
+import java.math.RoundingMode
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
+import kotlin.time.measureTime
+
+private fun compareStringBuilderAndIndexedStringBuilderPerformance(
+    printHeader: Boolean = false,
+    debugInformation: Boolean = false,
+    stringSize: Int,
+    fields: Int,
+    iterations: Int,
+    stringBuilderDone: (duration: Duration) -> Unit,
+    indexedStringBuilderDone: (duration: Duration) -> Unit
+) {
+    require(stringSize >= fields) { "stringSize must be greater than or equal to elements" }
+    require(stringSize % fields == 0) { "stringSize: $stringSize must a multiple of fields: $fields" }
+
+    if (printHeader) {
+        println("stringSize, ${if (debugInformation) "fields, " else "" }chunksSize, ratio${if (debugInformation) ", duration(ms)" else "" }")
+    }
+
+    print("$stringSize, ${if (debugInformation) "$fields, " else ""}${stringSize / fields}")
+
+    val replacingString = "b".repeat(stringSize / fields)
+    val sbDuration = measureTime {
+        val sb = StringBuilder("a".repeat(stringSize))
+        for (i in 0 until iterations) {
+            val replacingChars = stringSize / fields
+            for (j in 0 until fields) {
+                val start: Int = j * replacingChars
+                val end: Int = start + replacingChars
+                sb.replace(start, end, replacingString)
+            }
+        }
+    }
+    stringBuilderDone(sbDuration)
+
+    val indexedSbDuration = measureTime {
+        val indexedSb = IndexedStringBuilder("a".repeat(stringSize), stringSize / fields)
+        for (i in 0 until iterations) {
+            val replacingChars = stringSize / fields
+            for (j in 0 until fields) {
+                val start: Int = j * replacingChars
+                val end: Int = start + replacingChars
+                indexedSb.replace(start, end, replacingString)
+            }
+        }
+    }
+
+    indexedStringBuilderDone(indexedSbDuration)
+
+    val ratio = sbDuration.div(indexedSbDuration).toBigDecimal().setScale(2, RoundingMode.HALF_DOWN).toDouble()
+
+    println(", $ratio${if (debugInformation) ", ${sbDuration.plus(indexedSbDuration).toLong(DurationUnit.MILLISECONDS)}" else ""}")
+}
+
+private fun createPerformanceComparisonDataset() {
+    for (stringSize in listOf(10, 100, 500, 1000, 2_000, 5_000, 10_000, 50_000, 100_000, 1_000_000, 10_000_000)) {
+        for (fields in listOf(1, 2, 5, 10, 20, 50, 100, 200, 500, 1000)) {
+            if (stringSize < fields || ((stringSize % fields) != 0)) {
+                break
+            }
+            compareStringBuilderAndIndexedStringBuilderPerformance(
+                printHeader = stringSize == 10 && fields == 1,
+                stringSize = stringSize,
+                fields = fields,
+                iterations = if (stringSize*fields < 100_000) 1_000_000 else if (stringSize*fields < 1_000_000) 100_000 else 100,
+                stringBuilderDone = { },
+                indexedStringBuilderDone = { }
+            )
+        }
+    }
+}
+
+fun main() {
+    createPerformanceComparisonDataset()
+}
--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/interpreter/IndexedStringBuilderTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/interpreter/IndexedStringBuilderTest.kt
@@ -3,8 +3,6 @@ package com.smeup.rpgparser.interpreter
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
-import kotlin.time.DurationUnit
-import kotlin.time.measureTime
 
 class IndexedStringBuilderTest {
 
@@ -86,90 +84,4 @@ class IndexedStringBuilderTest {
         val result = builder.substring(5, 5)
         assertEquals("", result)
     }
-
-    private fun compareStringBuilderAndIndexedStringBuilderPerformance(descriptionTest: String, stringSize: Int, iterations: Int) {
-        println("Test: $descriptionTest")
-        require(stringSize % 10 == 0) { "stringSize must be a multiple of 10" }
-        val replacingString = "b".repeat(stringSize / 10)
-        val sbDuration = measureTime {
-            for (i in 0 until iterations) {
-                val sb = StringBuilder("a".repeat(stringSize))
-                val step = stringSize / 10
-                for (j in 0 until step) {
-                    val start: Int = j * step
-                    val end: Int = start + step
-                    sb.replace(start, end, replacingString)
-                }
-            }
-        }
-
-        val indexedSbDuration = measureTime {
-            for (i in 0 until iterations) {
-                val indexedSb = IndexedStringBuilder("a".repeat(stringSize), stringSize / 10)
-                val step = stringSize / 10
-                for (j in 0 until step) {
-                    val start: Int = j*step
-                    val end: Int = start + step
-                    indexedSb.replace(start, end, replacingString)
-                }
-            }
-        }
-
-        println("StringBuilder: ${sbDuration.toLong(DurationUnit.MILLISECONDS)}ms")
-        println("IndexedStringBuilder: ${indexedSbDuration.toLong(DurationUnit.MILLISECONDS)}ms")
-    }
-
-    @Test
-    fun littleStringFewLoopPerformance() {
-        val stringSize = 100_000
-        val iterations = 100
-        compareStringBuilderAndIndexedStringBuilderPerformance("littleStringFewLoopPerformance", stringSize, iterations)
-    }
-
-//    @Test
-//    fun evalStringBuilderPerformances() {
-//        // DS elements
-//        val elements = 1_000
-//
-//        // Fields in each element
-//        val fields = 100
-//
-//        // Field size
-//        val fieldSize = 100
-//
-//        // DS value as StringBuilder
-//        val sb = StringBuilder("a".repeat(elements*fields*fieldSize))
-//
-//        // DS value as Indexed StringBuilder
-//        val indexedSb = MutableList(elements) { StringBuilder("a".repeat(fields*fieldSize)) }
-//
-//        val replacingString = "b".repeat(fieldSize)
-//
-//        val randomField = List(fields) { (0..<fields).random() }
-//
-//        measureTime {
-//            // For each element of the list
-//            for (i in 0 until elements) {
-//                val elementOffset = i * fieldSize * fields
-//                for (field in 0 until fields) {
-//                    val start = elementOffset + (randomField[field] * fieldSize)
-//                    val end = start + fieldSize
-//                    sb.replace(start, end, replacingString)
-//                }
-//            }
-//        }.also { println("StringBuilder: ${it.toLong(DurationUnit.MILLISECONDS)}ms") }
-//
-//        measureTime {
-//            // For each element of the list
-//            for (i in 0 until elements) {
-//                for (field in 0 until fields) {
-//                    val start = randomField[field] * fieldSize
-//                    val end = start + fieldSize
-//                    indexedSb[i].replace(start, end, replacingString)
-//                }
-//            }
-//        }.also { println("Indexed StringBuilder: ${it.toLong(DurationUnit.MILLISECONDS)}ms") }
-//
-//        assertEquals(sb.toString(), indexedSb.joinToString(separator = ""))
-//    }
 }
