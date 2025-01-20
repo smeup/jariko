16dc1f76ea6d148b57226e8cc7c16b1ea139908c
Davide Palladino <davide.palladino@apuliasoft.com>
2025-01-20 13:41:33 +0100

    Implemented Smoke Test to validate DS fields (with numbers)

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/parsing/ast/ToAstSmokeTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/parsing/ast/ToAstSmokeTest.kt
@@ -17,11 +17,10 @@
 package com.smeup.rpgparser.parsing.ast
 
 import com.smeup.rpgparser.AbstractTest
-import com.smeup.rpgparser.interpreter.DataStructureType
-import com.smeup.rpgparser.interpreter.Scope
-import com.smeup.rpgparser.interpreter.Visibility
+import com.smeup.rpgparser.interpreter.*
 import com.smeup.rpgparser.parsing.parsetreetoast.DSFieldInitKeywordType
 import com.smeup.rpgparser.parsing.parsetreetoast.resolveAndValidate
+import com.smeup.rpgparser.parsing.parsetreetoast.toDataDefinitions
 import org.junit.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -394,6 +393,24 @@ open class ToAstSmokeTest : AbstractTest() {
             this.resolveAndValidate()
             assertNotNull(this.getAnyDataDefinition("SQLCOD"), "SQLCOD is defined")
             assertNotNull(this.getAnyDataDefinition("SQLERM"), "SQLERM is defined")
+        }
+    }
+
+    /**
+     * Test the declaration of a DS which uses `EXTNAME` to a file already declared as `F` specification.
+     * The purpose of this test is to ensure about the right number of fields to avoid duplications.
+     */
+    @Test
+    fun buildAstForEXTNAMEDS01() {
+        assertASTCanBeProduced(exampleName = "EXTNAMEDS01", printTree = false).apply {
+            this.resolveAndValidate()
+
+            val dataStructure: DataDefinition = this.getAnyDataDefinition("DS_ST01") as DataDefinition
+
+            assertEquals(dataStructure.fields.size, 4)
+            assertNotNull(dataStructure.fields.firstOrNull { fieldDefinition -> fieldDefinition.name.equals("ST01_KEY") }, "ST01_KEY is defined under DS_ST01")
+            assertNotNull(dataStructure.fields.firstOrNull { fieldDefinition -> fieldDefinition.name.equals("ST01_COL1") }, "ST01_COL1 is defined under DS_ST01")
+            assertNotNull(dataStructure.fields.firstOrNull { fieldDefinition -> fieldDefinition.name.equals("ST01_COL2") }, "ST01_COL2 is defined under DS_ST01")
         }
     }
 }
