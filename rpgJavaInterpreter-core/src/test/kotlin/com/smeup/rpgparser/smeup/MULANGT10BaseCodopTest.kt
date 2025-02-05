a2a1bbd47732f5fc411321a621a5c8f127066cba
Davide Palladino <davide.palladino@apuliasoft.com>
2025-02-05 13:49:23 +0100

    Fixed expected value

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT10BaseCodopTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT10BaseCodopTest.kt
@@ -935,7 +935,7 @@ open class MULANGT10BaseCodopTest : MULANGTTest() {
      */
     @Test
     fun executeMUDRNRAPU00193() {
-        val expected = listOf("0123456789", "", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+        val expected = listOf("0123456789", "", "0123456789", "0123456789")
         assertEquals(expected, "smeup/MUDRNRAPU00193".outputOf())
     }
 }
