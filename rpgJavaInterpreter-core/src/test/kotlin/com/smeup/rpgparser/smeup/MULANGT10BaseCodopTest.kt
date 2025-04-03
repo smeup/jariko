a2df2a303fe83ff71df4ebc8329d675f5f5cc7b9
Davide Palladino <davide.palladino@apuliasoft.com>
2025-04-03 15:54:30 +0200

    Removed `MUDRNRAPU001109` as `@Ignored`

--- rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT10BaseCodopTest.kt
+++ rpgJavaInterpreter-core/src/test/kotlin/com/smeup/rpgparser/smeup/MULANGT10BaseCodopTest.kt
@@ -1060,7 +1060,6 @@ open class MULANGT10BaseCodopTest : MULANGTTest() {
      * @see #LS25001579
      */
     @Test
-    @Ignore
     fun executeMUDRNRAPU001109() {
         val expected = listOf("1.50", "2.50", "3.50", "1.50", "2.50", "3.50")
         assertEquals(expected, "smeup/MUDRNRAPU001109".outputOf())
