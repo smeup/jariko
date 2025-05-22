94eedbb4a9e8253e9f4084ca751811b143b15900
lanarimarco <marco.lanari@smeup.com>
2025-05-22 17:20:32 +0200

    refactor: remove BOMInputStream because apparently unhelpful

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/execution/runner.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/execution/runner.kt
@@ -5,14 +5,13 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      https://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.smeup.rpgparser.execution
@@ -36,7 +35,6 @@ import com.smeup.rpgparser.parsing.ast.CompilationUnit
 import com.smeup.rpgparser.parsing.facade.Copy
 import com.smeup.rpgparser.parsing.facade.CopyId
 import com.smeup.rpgparser.rpginterop.*
-import org.apache.commons.io.input.BOMInputStream
 import java.io.File
 
 class CommandLineParms internal constructor(
@@ -117,7 +115,7 @@ class ResourceProgramFinder(val path: String) : RpgProgramFinder {
     override fun findRpgProgram(nameOrSource: String): RpgProgram? {
         val resourceStream = ResourceProgramFinder::class.java.getResourceAsStream("$path$nameOrSource.rpgle")
         return if (resourceStream != null) {
-            RpgProgram.fromInputStream(BOMInputStream(resourceStream), nameOrSource)
+            RpgProgram.fromInputStream(resourceStream, nameOrSource)
         } else {
             null
         }
