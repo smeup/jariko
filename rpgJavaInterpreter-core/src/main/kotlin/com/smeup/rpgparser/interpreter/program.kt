27eb0f539f1660c5476e49d5c7e20e0ed3c9de0a
domenico <domenico.mancini@apuliasoft.com>
2025-07-09 15:44:53 +0200

    Make RpgProgram traces description more explicit

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/program.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/program.kt
@@ -87,7 +87,7 @@ class RpgProgram(val cu: CompilationUnit, val name: String = "<UNNAMED RPG PROGR
 
     override fun execute(systemInterface: SystemInterface, params: LinkedHashMap<String, Value>): List<Value> {
         val callback = configuration.jarikoCallback
-        val trace = JarikoTrace(JarikoTraceKind.RpgProgram, this.name)
+        val trace = JarikoTrace(JarikoTraceKind.RpgProgram, "Execute ${this.name}")
         return callback.traceBlock(trace) {
             val expectedKeys = params().asSequence().map { it.name }.toSet()
 
