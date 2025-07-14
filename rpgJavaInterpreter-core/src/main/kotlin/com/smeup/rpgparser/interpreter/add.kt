c0096ababd6431f521d6f4a985ca84fd6a8bc850
Davide Palladino <davide.palladino@apuliasoft.com>
2025-07-14 17:09:44 +0200

    Applied fix for `MUDRNRAPU001140` test

--- rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/add.kt
+++ rpgJavaInterpreter-core/src/main/kotlin/com/smeup/rpgparser/interpreter/add.kt
@@ -45,10 +45,53 @@ fun add(
     }
 
     return when {
+        addendOneValue is ArrayValue && addendTwoValue is ArrayValue -> makeArrayValue(addendOneValue, addendTwoValue, position)
+        addendOneValue is ArrayValue && addendTwoValue is NumberValue -> TODO()
+        else -> makeSingleValue(addendOneValue, addendTwoValue, position)
+    }
+}
+
+/**
+ * Combines the values of two `Value` objects into a single `Value`,
+ * performing addition based on their types.
+ *
+ * @param addendOneValue the first `Value` to be added; must be of type `IntValue` or `DecimalValue`
+ * @param addendTwoValue the second `Value` to be added; must be of type `IntValue` or `DecimalValue`
+ * @param position optional `Position` for contextual information during operation; used in exception message if applicable
+ * @return a newly created `Value` resulting from the addition of `addendOneValue` and `addendTwoValue`,
+ *         which will be either an `IntValue` or a `DecimalValue` depending on the input types
+ * @throws UnsupportedOperationException if the provided `Value` types cannot be summed or are unsupported
+ */
+private fun makeSingleValue(addendOneValue: Value, addendTwoValue: Value, position: Position?): Value {
+    return when {
         addendOneValue is IntValue && addendTwoValue is IntValue -> IntValue(addendOneValue.asInt().value.plus(addendTwoValue.asInt().value))
         addendOneValue is IntValue && addendTwoValue is DecimalValue -> DecimalValue(addendOneValue.asDecimal().value.plus(addendTwoValue.value))
         addendOneValue is DecimalValue && addendTwoValue is IntValue -> DecimalValue(addendOneValue.value.plus(addendTwoValue.asDecimal().value))
         addendOneValue is DecimalValue && addendTwoValue is DecimalValue -> DecimalValue(addendOneValue.value.plus(addendTwoValue.value))
         else -> throw UnsupportedOperationException("I do not know how to sum $addendOneValue and $addendTwoValue at $position")
     }
+}
+
+/**
+ * Creates a new `ArrayValue` by combining the elements of two input `ArrayValue` objects.
+ * The operation is element-wise and applies up to the minimum size of the two arrays.
+ *
+ * @param addendOneValue the first `ArrayValue` instance whose elements will be processed
+ * @param addendTwoValue the second `ArrayValue` instance whose elements will be combined
+ * @param position an optional `Position` object providing context for error reporting
+ * @return a new `ArrayValue` containing the combined elements
+ */
+private fun makeArrayValue(addendOneValue: ArrayValue, addendTwoValue: ArrayValue, position: Position?): ArrayValue {
+    val minSize = minOf(addendOneValue.arrayLength(), addendTwoValue.arrayLength())
+    val newArrayValue = addendTwoValue.copy()
+
+    1.rangeTo(minSize)
+        .forEach { index ->
+            newArrayValue.setElement(
+                index,
+                makeSingleValue(addendOneValue.getElement(index), newArrayValue.getElement(index), position)
+            )
+        }
+
+    return newArrayValue
 }
